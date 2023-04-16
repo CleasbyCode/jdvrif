dbee97cb39917116a14542ee32e68bfcdec37198
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2023-04-16 17:31:16 +0100

    Update jdvrdt_no_comments.cpp

--- src/jdvrdt_no_comments.cpp
+++ src/jdvrdt_no_comments.cpp
@@ -126,20 +126,28 @@ void processEmbeddedImage(char* argv[]) {
   
 	ImageVec.erase(ImageVec.begin(), ImageVec.begin() + deflateDataIndex);
 
-	std::vector<unsigned char> DQT_SIG{ 0xFF, 0xDB, 0x00, 0x43 };
-	
-	ptrdiff_t 
-		findProfileSigIndex = search(ImageVec.begin(), ImageVec.end(), PROFILE_SIG.begin(), PROFILE_SIG.end()) - ImageVec.begin() - 4,
-		dqtFirstPos = search(ImageVec.begin(), ImageVec.end(), DQT_SIG.begin(), DQT_SIG.end()) - ImageVec.begin();
+	std::vector<unsigned char> DQT_SIG_A{ 0xFF, 0xDB, 0x00, 0x43 };
+	std::vector<unsigned char> DQT_SIG_B{ 0xFF, 0xDB, 0x00, 0x84 };
 	
 	std::cout << "\nSearching for embedded data file. Please wait...\n";
-
-	while (dqtFirstPos >= findProfileSigIndex) {
+	
+	// Within "ImageVec", find and erase all occurrences of the contents of "ProfileChunkVec".
+	// Stop the search once we get to location of dqtIndex.
+	ptrdiff_t 
+		findProfileSigIndex = search(ImageVec.begin(), ImageVec.end(), PROFILE_SIG.begin(), PROFILE_SIG.end()) - ImageVec.begin() - 4,
+		dqtFirstPosA = search(ImageVec.begin(), ImageVec.end(), DQT_SIG_A.begin(), DQT_SIG_A.end()) - ImageVec.begin(),
+		dqtFirstPosB = search(ImageVec.begin(), ImageVec.end(), DQT_SIG_B.begin(), DQT_SIG_B.end()) - ImageVec.begin(),
+		dqtIndex = 0;
+	
+	dqtIndex = dqtFirstPosB > dqtFirstPosA ? dqtFirstPosA : dqtFirstPosB;
+	
+	while (dqtIndex > findProfileSigIndex) {
 		ImageVec.erase(ImageVec.begin() + findProfileSigIndex, ImageVec.begin() + findProfileSigIndex + 18);
 		findProfileSigIndex = search(ImageVec.begin(), ImageVec.end(), PROFILE_SIG.begin(), PROFILE_SIG.end()) - ImageVec.begin() - 4;
 	}
 	
-	dqtFirstPos = search(ImageVec.begin(), ImageVec.end(), DQT_SIG.begin(), DQT_SIG.end()) - ImageVec.begin();
+	dqtIndex = dqtIndex == dqtFirstPosA ? search(ImageVec.begin(), ImageVec.end(), DQT_SIG_A.begin(), DQT_SIG_A.end()) - ImageVec.begin() 
+		: search(ImageVec.begin(), ImageVec.end(), DQT_SIG_B.begin(), DQT_SIG_B.end()) - ImageVec.begin();
 
 	ImageVec.erase(ImageVec.begin() + dqtFirstPos, ImageVec.end());
 
@@ -334,7 +342,7 @@ void inflateDeflate(std::vector<unsigned char>& Vec, bool inflateData) {
 		inflateInit(&strm);
 	}
 	else {
-		deflateInit(&strm, 6); 
+		deflateInit(&strm, 9); 
 	}
 
 	while (strm.avail_in)
