e74cb531a4ebb732f8bfd2d1c0553e452de3e157
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-17 15:47:29 +0100

    Update jdvin.h

--- src/jdvin/jdvin.h
+++ src/jdvin/jdvin.h
@@ -8,9 +8,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <zlib.h>
 
 #include "value_updater.cpp"
 #include "encrypt.cpp"
+#include "deflate.cpp"
 #include "insert_profile_headers.cpp"
 #include "information.cpp"
 #include "jdvin.cpp"
@@ -18,7 +20,7 @@
 void
 	startJdv(std::string&, std::string&, bool),
 	encryptFile(std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&, std::string&),
+	deflateFile(std::vector<uint_fast8_t>&),
 	insertProfileHeaders(std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&, uint_fast32_t, bool),
 	Value_Updater(std::vector<uint_fast8_t>&, uint_fast32_t, const uint_fast32_t, uint_fast8_t),
 	displayInfo();
-
