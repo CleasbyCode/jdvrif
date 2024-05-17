663603af9e43284321d65b31c30aae676c0e9325
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-17 11:11:18 +0100

    Update information.cpp

--- src/jdvin/information.cpp
+++ src/jdvin/information.cpp
@@ -18,8 +18,8 @@ Usage: jdvin [-r] <cover_image> <data_file>
 Post your file-embedded image on the following compatible sites.
 *Image size limits(cover image + data file):
 
-  Flickr (200MB), *ImgPile (100MB), ImgBB (32MB), PostImage (24MB), *Reddit (20MB / requires -r option), 
-  Mastodon (16MB), *X/Twitter (~10KB / *Limit measured by data file size).
+Flickr (200MB), *ImgPile (100MB), ImgBB (32MB), PostImage (24MB), *Reddit (20MB / requires -r option), 
+Mastodon (16MB), *X/Twitter (~10KB / *Limit measured by data file size).
 
 Arguments / options:	
 		
