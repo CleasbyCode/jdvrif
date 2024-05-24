83f579317b03971b6abc80aab33c29c8a59bf303
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-24 18:44:31 +0100

    Update main.cpp

--- src/jdvin/main.cpp
+++ src/jdvin/main.cpp
@@ -1,10 +1,11 @@
-//	JPG Data Vehicle (jdvin v1.0.2) Created by Nicholas Cleasby (@CleasbyCode) 10/04/2023
+
+//	JPG Data Vehicle (jdvin v1.0.3) Created by Nicholas Cleasby (@CleasbyCode) 10/04/2023
 //
 //	To compile program (Linux):
-// 	$ g++ jdvrif.cpp -O2 -lz -s -o jdvrif
+// 	$ g++ main.cpp -O2 -lz -s -o jdvin
 
 // 	Run it:
-// 	$ ./jdvrif
+// 	$ ./jdvin
 
 #include "jdvin.h"
 
