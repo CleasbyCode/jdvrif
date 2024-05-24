64bf5e2855901b25820dafca51d32498b7e1c67e
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-24 18:51:39 +0100

    Update find_profile_headers.cpp

--- src/jdvout/find_profile_headers.cpp
+++ src/jdvout/find_profile_headers.cpp
@@ -1,3 +1,4 @@
+
 void findProfileHeaders(std::vector<uint_fast8_t>&Image_Vec, std::vector<uint_fast32_t>&Profile_Headers_Offset_Vec, std::string& file_name) {
 
 	constexpr uint_fast8_t	
@@ -34,6 +35,5 @@ void findProfileHeaders(std::vector<uint_fast8_t>&Image_Vec, std::vector<uint_fa
 			Profile_Headers_Offset_Vec.emplace_back(profile_header_index = static_cast<uint_fast32_t>(std::search(Image_Vec.begin() + profile_header_index + 5, Image_Vec.end(), 									std::begin(ICC_PROFILE_SIG), std::end(ICC_PROFILE_SIG)) - Image_Vec.begin() - 4));
 		}
 	}
-
 	Image_Vec.erase(Image_Vec.begin() + EMBEDDED_FILE_SIZE, Image_Vec.end());
 }
