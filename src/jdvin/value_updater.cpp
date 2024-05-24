6e58743b947bb22dd790da82e76afecf1006d09f
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-24 18:31:27 +0100

    Delete src/jdvin directory

--- src/jdvin/value_updater.cpp
+++
@@ -1,7 +0,0 @@
-// Writes updated values (two bytes or four bytes), such as block/chunk lengths etc. into the relevant vector index locations for the file.
-
-void Value_Updater(std::vector<uint_fast8_t>& vec, uint_fast32_t value_insert_index, const uint_fast32_t VALUE, uint_fast8_t bits) {
-	while (bits) {
-		static_cast<uint_fast32_t>(vec[value_insert_index++] = (VALUE >> (bits -= 8)) & 0xff);
-	}
-}
