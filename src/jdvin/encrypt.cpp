7d6d2b23c8f8017a2f53dba8eed1d85446930273
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-29 22:36:53 +0100

    Update encrypt.cpp

--- src/jdvin/encrypt.cpp
+++ src/jdvin/encrypt.cpp
@@ -1,4 +1,4 @@
-	void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_t>& File_Vec, std::string& data_file_name) {
+void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_t>& File_Vec, std::string& data_file_name) {
 	
 	std::random_device rd;
  	std::mt19937 gen(rd());
@@ -22,15 +22,14 @@
 		xor_key[XOR_KEY_LENGTH],
 		xor_key_pos{},
 		name_key_pos{};
-
+	
 	for (int i = 0; i < XOR_KEY_LENGTH; ++i) {
         	xor_key[i] = static_cast<uint_fast8_t>(dis(gen));
     	}
-
+	
 	std::copy(std::begin(xor_key), std::end(xor_key), Profile_Vec.begin() + PROFILE_XOR_KEY_INSERT_INDEX);
 
 	while (DATA_FILE_SIZE > index_pos) {
-
 		if (index_pos >= DATA_FILE_NAME_LENGTH) {
 			name_key_pos = name_key_pos >= DATA_FILE_NAME_LENGTH ? 0 : name_key_pos;
 		} else {
@@ -39,10 +38,8 @@
 			xor_key_pos = xor_key_pos >= XOR_KEY_LENGTH ? 0 : xor_key_pos;
 			data_file_name[index_pos] = data_file_name[index_pos] ^ xor_key[xor_key_pos++];
 		}
-
 		Profile_Vec.emplace_back(File_Vec[index_pos++] ^ data_file_name[name_key_pos++]);			
 	}
-
-		Profile_Vec[PROFILE_NAME_INDEX - 1] = DATA_FILE_NAME_LENGTH;
-		std::copy(data_file_name.begin(), data_file_name.end(), Profile_Vec.begin() + PROFILE_NAME_INDEX);
+	Profile_Vec[PROFILE_NAME_INDEX - 1] = DATA_FILE_NAME_LENGTH;
+	std::copy(data_file_name.begin(), data_file_name.end(), Profile_Vec.begin() + PROFILE_NAME_INDEX);
 }
