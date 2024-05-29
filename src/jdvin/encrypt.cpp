c5aa96e28729603681ef7d633958453c3457dc3e
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-29 22:31:52 +0100

    Add files via upload

---
+++ src/jdvin/encrypt.cpp
@@ -0,0 +1,48 @@
+	void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_t>& File_Vec, std::string& data_file_name) {
+	
+	std::random_device rd;
+ 	std::mt19937 gen(rd());
+	std::uniform_int_distribution<unsigned short> dis(1, 255); 
+	
+	constexpr uint_fast16_t 
+		PROFILE_XOR_KEY_INSERT_INDEX = 0x260,
+		PROFILE_VEC_SIZE = 663;	
+
+	constexpr uint_fast8_t 
+		XOR_KEY_LENGTH = 12,
+		PROFILE_NAME_INDEX = 0x51;
+
+	const uint_fast32_t DATA_FILE_SIZE = static_cast<uint_fast32_t>(File_Vec.size());
+
+	const uint_fast8_t DATA_FILE_NAME_LENGTH = static_cast<uint_fast8_t>(data_file_name.length());
+       
+	uint_fast32_t index_pos{};
+
+	uint_fast8_t 
+		xor_key[XOR_KEY_LENGTH],
+		xor_key_pos{},
+		name_key_pos{};
+
+	for (int i = 0; i < XOR_KEY_LENGTH; ++i) {
+        	xor_key[i] = static_cast<uint_fast8_t>(dis(gen));
+    	}
+
+	std::copy(std::begin(xor_key), std::end(xor_key), Profile_Vec.begin() + PROFILE_XOR_KEY_INSERT_INDEX);
+
+	while (DATA_FILE_SIZE > index_pos) {
+
+		if (index_pos >= DATA_FILE_NAME_LENGTH) {
+			name_key_pos = name_key_pos >= DATA_FILE_NAME_LENGTH ? 0 : name_key_pos;
+		} else {
+			std::reverse(std::begin(xor_key), std::end(xor_key));
+
+			xor_key_pos = xor_key_pos >= XOR_KEY_LENGTH ? 0 : xor_key_pos;
+			data_file_name[index_pos] = data_file_name[index_pos] ^ xor_key[xor_key_pos++];
+		}
+
+		Profile_Vec.emplace_back(File_Vec[index_pos++] ^ data_file_name[name_key_pos++]);			
+	}
+
+		Profile_Vec[PROFILE_NAME_INDEX - 1] = DATA_FILE_NAME_LENGTH;
+		std::copy(data_file_name.begin(), data_file_name.end(), Profile_Vec.begin() + PROFILE_NAME_INDEX);
+}
---
+++ src/jdvin/main.cpp
@@ -0,0 +1,51 @@
+//	JPG Data Vehicle (jdvin v1.0.3) Created by Nicholas Cleasby (@CleasbyCode) 10/04/2023
+//
+//	To compile program (Linux):
+// 	$ g++ main.cpp -O2 -lz -s -o jdvin
+
+// 	Run it:
+// 	$ ./jdvin
+
+#include "jdvin.h"
+
+int main(int argc, char** argv) {
+
+	if (argc == 2 && std::string(argv[1]) == "--info") {
+		displayInfo();
+	} else if (argc > 2 && argc < 5) {
+		bool 
+			isFileCheckSuccess = false,
+			isRedditOption = argc > 3 ? true : false;
+
+		const std::regex REG_EXP("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+
+		const std::string IMAGE_FILE_NAME = isRedditOption ? argv[2] : argv[1];
+
+		std::string 
+			image_file_extension = IMAGE_FILE_NAME.length() > 3 ? IMAGE_FILE_NAME.substr(IMAGE_FILE_NAME.length() - 4) : IMAGE_FILE_NAME,
+			data_file_name = isRedditOption ? argv[3] : argv[2];
+
+		image_file_extension = image_file_extension == "jpeg" || image_file_extension == "jiff" ? ".jpg" : image_file_extension;
+
+		if (image_file_extension != ".jpg" || (argc > 3 && std::string(argv[1]) != "-r")) {
+			std::cerr << (image_file_extension != ".jpg" 
+				? "\nFile Type Error: Invalid file extension. Expecting only \"jpg\""
+				: "\nInput Error: Invalid arguments. Expecting only -r") 
+			<< ".\n\n";
+		} else if (!regex_match(IMAGE_FILE_NAME, REG_EXP) || !regex_match(data_file_name, REG_EXP)) {
+			std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
+		} else if (!std::filesystem::exists(IMAGE_FILE_NAME) || !std::filesystem::exists(data_file_name)) {
+			std::cerr << (!std::filesystem::exists(IMAGE_FILE_NAME) 
+				? "\nImage"
+				: "\nData") 
+			<< " File Error: File not found. Check the filename and try again.\n\n";
+		} else {
+			isFileCheckSuccess = true;
+		}
+		isFileCheckSuccess ? startJdv(IMAGE_FILE_NAME, data_file_name, isRedditOption) : std::exit(EXIT_FAILURE);	  	
+	}
+	else {
+		std::cout << "\nUsage: jdvin [-r] <cover_image> <data_file>\n\t\bjdvin --info\n\n";
+	}
+     return 0;	
+}
