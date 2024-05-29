70479a3470dfea446a8c33363861f09419377a4f
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-29 22:30:07 +0100

    Delete src/jdvin directory

--- src/jdvin/value_updater.cpp
+++
@@ -1,6 +0,0 @@
-
-void Value_Updater(std::vector<uint_fast8_t>& vec, uint_fast32_t value_insert_index, const uint_fast32_t VALUE, uint_fast8_t bits) {
-	while (bits) {
-		static_cast<uint_fast32_t>(vec[value_insert_index++] = (VALUE >> (bits -= 8)) & 0xff);
-	}
-}
