618325145a636d6ff059b58cf688fe2953c30a35
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-30 13:49:16 +0100

    Update encrypt.cpp

--- src/jdvin/encrypt.cpp
+++ src/jdvin/encrypt.cpp
@@ -1,4 +1,4 @@
-void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_t>& File_Vec, std::string& data_file_name) {
+void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_t>& File_Vec, std::string& data_filename) {
 	
 	std::random_device rd;
  	std::mt19937 gen(rd());
@@ -14,7 +14,7 @@ void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_
 
 	const uint_fast32_t DATA_FILE_SIZE = static_cast<uint_fast32_t>(File_Vec.size());
 
-	const uint_fast8_t DATA_FILE_NAME_LENGTH = static_cast<uint_fast8_t>(data_file_name.length());
+	const uint_fast8_t DATA_FILENAME_LENGTH = static_cast<uint_fast8_t>(data_filename.length());
        
 	uint_fast32_t index_pos{};
 
@@ -30,16 +30,16 @@ void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_
 	std::copy(std::begin(xor_key), std::end(xor_key), Profile_Vec.begin() + PROFILE_XOR_KEY_INSERT_INDEX);
 
 	while (DATA_FILE_SIZE > index_pos) {
-		if (index_pos >= DATA_FILE_NAME_LENGTH) {
-			name_key_pos = name_key_pos >= DATA_FILE_NAME_LENGTH ? 0 : name_key_pos;
+		if (index_pos >= DATA_FILENAME_LENGTH) {
+			name_key_pos = name_key_pos >= DATA_FILENAME_LENGTH ? 0 : name_key_pos;
 		} else {
 			std::reverse(std::begin(xor_key), std::end(xor_key));
 
 			xor_key_pos = xor_key_pos >= XOR_KEY_LENGTH ? 0 : xor_key_pos;
-			data_file_name[index_pos] = data_file_name[index_pos] ^ xor_key[xor_key_pos++];
+			data_filename[index_pos] = data_filename[index_pos] ^ xor_key[xor_key_pos++];
 		}
-		Profile_Vec.emplace_back(File_Vec[index_pos++] ^ data_file_name[name_key_pos++]);			
+		Profile_Vec.emplace_back(File_Vec[index_pos++] ^ data_filename[name_key_pos++]);			
 	}
-	Profile_Vec[PROFILE_NAME_INDEX - 1] = DATA_FILE_NAME_LENGTH;
-	std::copy(data_file_name.begin(), data_file_name.end(), Profile_Vec.begin() + PROFILE_NAME_INDEX);
+	Profile_Vec[PROFILE_NAME_INDEX - 1] = DATA_FILENAME_LENGTH;
+	std::copy(data_filename.begin(), data_filename.end(), Profile_Vec.begin() + PROFILE_NAME_INDEX);
 }
