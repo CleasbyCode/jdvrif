0231c0ab530c813d97a2ff34b6ab2c0c1568e295
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-05-30 13:52:08 +0100

    Update main.cpp

--- src/jdvin/main.cpp
+++ src/jdvin/main.cpp
@@ -19,11 +19,11 @@ int main(int argc, char** argv) {
 
 		const std::regex REG_EXP("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
 
-		const std::string IMAGE_FILE_NAME = isRedditOption ? argv[2] : argv[1];
+		const std::string IMAGE_FILENAME = isRedditOption ? argv[2] : argv[1];
 
 		std::string 
-			image_file_extension = IMAGE_FILE_NAME.length() > 3 ? IMAGE_FILE_NAME.substr(IMAGE_FILE_NAME.length() - 4) : IMAGE_FILE_NAME,
-			data_file_name = isRedditOption ? argv[3] : argv[2];
+			image_file_extension = IMAGE_FILENAME.length() > 3 ? IMAGE_FILENAME.substr(IMAGE_FILENAME.length() - 4) : IMAGE_FILENAME,
+			data_filename = isRedditOption ? argv[3] : argv[2];
 
 		image_file_extension = image_file_extension == "jpeg" || image_file_extension == "jiff" ? ".jpg" : image_file_extension;
 
@@ -32,17 +32,17 @@ int main(int argc, char** argv) {
 				? "\nFile Type Error: Invalid file extension. Expecting only \"jpg\""
 				: "\nInput Error: Invalid arguments. Expecting only -r") 
 			<< ".\n\n";
-		} else if (!regex_match(IMAGE_FILE_NAME, REG_EXP) || !regex_match(data_file_name, REG_EXP)) {
+		} else if (!regex_match(IMAGE_FILENAME, REG_EXP) || !regex_match(data_filename, REG_EXP)) {
 			std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
-		} else if (!std::filesystem::exists(IMAGE_FILE_NAME) || !std::filesystem::exists(data_file_name)) {
-			std::cerr << (!std::filesystem::exists(IMAGE_FILE_NAME) 
+		} else if (!std::filesystem::exists(IMAGE_FILENAME) || !std::filesystem::exists(data_filename)) {
+			std::cerr << (!std::filesystem::exists(IMAGE_FILENAME) 
 				? "\nImage"
 				: "\nData") 
 			<< " File Error: File not found. Check the filename and try again.\n\n";
 		} else {
 			isFileCheckSuccess = true;
 		}
-		isFileCheckSuccess ? startJdv(IMAGE_FILE_NAME, data_file_name, isRedditOption) : std::exit(EXIT_FAILURE);	  	
+		isFileCheckSuccess ? startJdv(IMAGE_FILENAME, data_filename, isRedditOption) : std::exit(EXIT_FAILURE);	  	
 	} else {
 		std::cout << "\nUsage: jdvin [-r] <cover_image> <data_file>\n\t\bjdvin --info\n\n";
 	}
