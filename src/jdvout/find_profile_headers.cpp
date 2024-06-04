e1a75fa3b48f552b64524d897eae004ad7e9d1c4
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-04 12:44:36 +0100

    Update find_profile_headers.cpp

--- src/jdvout/find_profile_headers.cpp
+++ src/jdvout/find_profile_headers.cpp
@@ -1,10 +1,12 @@
 void findProfileHeaders(std::vector<uint_fast8_t>&Image_Vec, std::vector<uint_fast32_t>&Profile_Headers_Offset_Vec, uint_fast16_t profile_count) {
 
-	constexpr uint_fast8_t ICC_PROFILE_SIG[11] { 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45};
-
+	constexpr uint_fast8_t 
+		ICC_PROFILE_SIG[11] { 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45},
+		PROFILE_HEADER_LENGTH = 18;
+				     
 	uint_fast32_t profile_header_index{}; 
 	
-	Profile_Headers_Offset_Vec.reserve(3500);
+	Profile_Headers_Offset_Vec.reserve(profile_count * PROFILE_HEADER_LENGTH);
 
 	while (profile_count--) {
 		profile_header_index = static_cast<uint_fast32_t>(
