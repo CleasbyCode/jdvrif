ace9b789890a5e87f66bea5a22a53ea1de2220b0
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-05 16:02:47 +0100

    Update information.cpp

--- src/jdvin/information.cpp
+++ src/jdvin/information.cpp
@@ -2,7 +2,7 @@ void displayInfo() {
 
 	std::cout << R"(
 
-JPG Data Vehicle (jdvin v1.0.3). 
+JPG Data Vehicle (jdvin v1.0.4). 
 Created by Nicholas Cleasby (@CleasbyCode) 24/01/2023.
 
 A steganography-like CLI tool to embed & hide any file type within a JPG image.  
