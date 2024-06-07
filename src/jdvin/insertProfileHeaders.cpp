391b19c35d4b41e0bbec4e9920f9a5b9762597ec
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-07 21:50:27 +0100

    Update insertProfileHeaders.cpp

--- src/jdvin/insertProfileHeaders.cpp
+++ src/jdvin/insertProfileHeaders.cpp
@@ -3,13 +3,15 @@ void insertProfileHeaders(std::vector<uint_fast8_t>&Profile_Vec, std::vector<uin
 	const uint_fast32_t PROFILE_VECTOR_SIZE = static_cast<uint_fast32_t>(Profile_Vec.size());	
 
 	constexpr uint_fast8_t
-		ICC_PROFILE_HEADER[] { 0xFF, 0xE2, 0xFF, 0xFF, 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45, 0x00, 0x01, 0x01 },
 		PROFILE_HEADER_LENGTH = 18,
 		PROFILE_HEADER_SIZE_INDEX = 0x16,	
 		PROFILE_SIZE_INDEX = 0x28,	
 		PROFILE_TALLY_INDEX = 0x8A,	
 		PROFILE_DATA_SIZE_INDEX = 0x90,
-		JPG_HEADER_LENGTH = 20;
+		JPG_HEADER_LENGTH = 20,
+		MASTODON_PROFILE_INSERT_LIMIT = 100;
+	
+	constexpr uint_fast16_t TWITTER_DATA_SIZE_LIMIT = 10240;
 
 	constexpr uint_fast32_t BLOCK_SIZE = 65519 + PROFILE_HEADER_LENGTH;
 	
@@ -23,12 +25,22 @@ void insertProfileHeaders(std::vector<uint_fast8_t>&Profile_Vec, std::vector<uin
 		valueUpdater(Profile_Vec, PROFILE_HEADER_SIZE_INDEX, PROFILE_HEADER_BLOCK_SIZE, bits);
 		valueUpdater(Profile_Vec, PROFILE_SIZE_INDEX, PROFILE_BLOCK_SIZE, bits);
 
+		if (PROFILE_HEADER_BLOCK_SIZE > TWITTER_DATA_SIZE_LIMIT) {
+			std::cout << "\nWarning: Output image is not compatible with X/Twitter. Embedded data file exceeds the size limit.\n";
+		}
+
 		File_Vec.swap(Profile_Vec);
 		
 	} else {
-		
+
+		std::cout << "\nWarning: Output image is not compatible with X/Twitter. Embedded data file exceeds the size limit.\n";
+
+		std::vector<uint_fast8_t>Profile_Header_Vec = { 0xFF, 0xE2, 0xFF, 0xFF, 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45, 0x00, 0x01, 0x01 };
+
 		uint_fast32_t 
 			byte_index{},
+			profile_count = 1,
+			profile_count_header_insert_index = 0x0F,
 			profile_header_insert_tally = PROFILE_VECTOR_SIZE / BLOCK_SIZE,
 			profile_header_total_byte_value = profile_header_insert_tally * PROFILE_HEADER_LENGTH,
 			block_tally = BLOCK_SIZE + JPG_HEADER_LENGTH,
@@ -39,7 +51,10 @@ void insertProfileHeaders(std::vector<uint_fast8_t>&Profile_Vec, std::vector<uin
 			File_Vec.emplace_back(Profile_Vec[byte_index++]);
 
 			if (byte_index == block_tally) {
-				File_Vec.insert(File_Vec.begin() + block_tally, std::begin(ICC_PROFILE_HEADER), std::end(ICC_PROFILE_HEADER));
+				profile_count++;
+				valueUpdater(Profile_Header_Vec, profile_count_header_insert_index, profile_count, bits);
+
+				File_Vec.insert(File_Vec.begin() + block_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
 				block_tally += BLOCK_SIZE;	
 			}
 		}
@@ -47,13 +62,19 @@ void insertProfileHeaders(std::vector<uint_fast8_t>&Profile_Vec, std::vector<uin
 		if (last_block_size > BLOCK_SIZE * 2) {
 			
 			last_block_size += (PROFILE_HEADER_LENGTH * 2) - 2;
-			
-			File_Vec.insert(File_Vec.begin() + block_tally, std::begin(ICC_PROFILE_HEADER), std::end(ICC_PROFILE_HEADER));
-			profile_header_insert_tally++;
+						
+			profile_count++;
+			valueUpdater(Profile_Header_Vec, profile_count_header_insert_index, profile_count, bits);
 
+			File_Vec.insert(File_Vec.begin() + block_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
+
+			profile_header_insert_tally++;
 			block_tally += BLOCK_SIZE;
 
-			File_Vec.insert(File_Vec.begin() + block_tally, std::begin(ICC_PROFILE_HEADER), std::end(ICC_PROFILE_HEADER));
+			profile_count++;
+			valueUpdater(Profile_Header_Vec, profile_count_header_insert_index, profile_count, bits);		
+
+			File_Vec.insert(File_Vec.begin() + block_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
 			profile_header_insert_tally++;
 
 			valueUpdater(File_Vec, block_tally + 2, last_block_size, bits);	
@@ -62,7 +83,10 @@ void insertProfileHeaders(std::vector<uint_fast8_t>&Profile_Vec, std::vector<uin
 			
 			last_block_size += PROFILE_HEADER_LENGTH - 1;
 			
-			File_Vec.insert(File_Vec.begin() + block_tally, std::begin(ICC_PROFILE_HEADER), std::end(ICC_PROFILE_HEADER));
+			profile_count++;
+			valueUpdater(Profile_Header_Vec, profile_count_header_insert_index, profile_count, bits);
+
+			File_Vec.insert(File_Vec.begin() + block_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
 			profile_header_insert_tally++;
 
 			valueUpdater(File_Vec, block_tally + 2, last_block_size, bits);	
@@ -74,6 +98,9 @@ void insertProfileHeaders(std::vector<uint_fast8_t>&Profile_Vec, std::vector<uin
 		}
 
 		valueUpdater(File_Vec, PROFILE_TALLY_INDEX, profile_header_insert_tally, bits);
+		if (profile_header_insert_tally + 1 > MASTODON_PROFILE_INSERT_LIMIT) {
+			std::cout << "\nWarning: Output image is not compatible with Mastodon. Embedded data file exceeds the size limit.\n";
+		}
 	}
 
 	bits = 32; 
