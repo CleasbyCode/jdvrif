a9a20d96c657be1c1a87e38870827f839dffc488
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-18 20:09:11 +0100

    Add files via upload

---
+++ src/jdvin/jdvin.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <algorithm>
+#include <filesystem>
+#include <random>
+#include <cstdint>
+#include <string_view>
+#include <fstream>
+#include <regex>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <C:\Users\Nick\source\zlib-1.3.1\zlib.h>
+
+#include "profilesVec.cpp"
+#include "writeOutFile.cpp"
+#include "searchFunc.cpp"
+#include "eraseSegments.cpp"
+#include "valueUpdater.cpp"
+#include "encryptFile.cpp"
+#include "deflateFile.cpp"
+
+#include "insertProfileHeaders.cpp"
+#include "information.cpp"
+#include "jdvin.cpp"
+
+template <uint_fast8_t N>
+uint_fast32_t searchFunc(std::vector<uint_fast8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
+
+void
+	startJdv(const std::string&, std::string&, bool),
+	eraseSegments(std::vector<uint_fast8_t>&, bool&),
+	deflateFile(std::vector<uint_fast8_t>&),
+	encryptFile(std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&, std::string&),
+	insertProfileHeaders(std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&),
+	writeOutFile(std::vector<uint_fast8_t>&),
+	valueUpdater(std::vector<uint_fast8_t>&, uint_fast32_t, const uint_fast32_t, uint_fast8_t),
+	displayInfo();
+
