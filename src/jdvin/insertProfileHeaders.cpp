84890f4f2bdda7a149c2a21537e8a20b7dccf7ec
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-06-26 12:19:01 +0100

    Update insertProfileHeaders.cpp

--- src/jdvin/insertProfileHeaders.cpp
+++ src/jdvin/insertProfileHeaders.cpp
@@ -1,101 +1,102 @@
+// If required, split and store data file into multiple color profile segment blocks.
 void insertProfileHeaders(std::vector<uint_fast8_t>&Profile_Vec, std::vector<uint_fast8_t>&File_Vec) {
-
 	constexpr uint_fast8_t
 		PROFILE_HEADER_LENGTH = 18,
-		PROFILE_HEADER_SIZE_INDEX = 0x16,	
-		PROFILE_SIZE_INDEX = 0x28,	
+		PROFILE_HEADER_SEGMENT_SIZE_INDEX = 0x16, // Two byte JPG color profile header segment size field index.	
+		PROFILE_SIZE_INDEX = 0x28,		  // Four byte profile size field index.	
 		PROFILE_TALLY_INDEX = 0x8A,	
-		DEFLATED_FILE_SIZE_INDEX = 0x90,
+		DEFLATED_DATA_FILE_SIZE_INDEX = 0x90,
 		JPG_HEADER_LENGTH = 20;
 
-	constexpr uint_fast16_t PROFILE_SIZE = 663;
-
-	constexpr uint_fast32_t BLOCK_SIZE = 65519 + PROFILE_HEADER_LENGTH;
+	constexpr uint_fast16_t COLOR_PROFILE_SIZE = 663;
+	constexpr uint_fast32_t SEGMENT_SIZE = 65537;
 
 	const uint_fast32_t 
-		PROFILE_VECTOR_SIZE = static_cast<uint_fast32_t>(Profile_Vec.size()),
-		DEFLATED_FILE_SIZE = PROFILE_VECTOR_SIZE - PROFILE_SIZE;
+		PROFILE_WITH_DATA_FILE_VEC_SIZE = static_cast<uint_fast32_t>(Profile_Vec.size()),
+		DEFLATED_DATA_FILE_SIZE = PROFILE_WITH_DATA_FILE_VEC_SIZE - COLOR_PROFILE_SIZE;
 	
 	uint_fast8_t bits = 16;	
 		
-	if (BLOCK_SIZE + JPG_HEADER_LENGTH >= PROFILE_VECTOR_SIZE) {
+	// Default profile and small data file fit within the first profile segment block.
+	if (SEGMENT_SIZE + JPG_HEADER_LENGTH >= PROFILE_WITH_DATA_FILE_VEC_SIZE) {
+		constexpr uint_fast8_t SIZE_DIFF = 16;
 		const uint_fast32_t
-			PROFILE_HEADER_BLOCK_SIZE = PROFILE_VECTOR_SIZE - (PROFILE_HEADER_LENGTH + 4),
-			PROFILE_BLOCK_SIZE = PROFILE_HEADER_BLOCK_SIZE - 16;
+			PROFILE_HEADER_SEGMENT_SIZE = PROFILE_WITH_DATA_FILE_VEC_SIZE - (PROFILE_HEADER_LENGTH + 4), 
+			PROFILE_SIZE = PROFILE_HEADER_SEGMENT_SIZE - SIZE_DIFF;
 
-		valueUpdater(Profile_Vec, PROFILE_HEADER_SIZE_INDEX, PROFILE_HEADER_BLOCK_SIZE, bits);
-		valueUpdater(Profile_Vec, PROFILE_SIZE_INDEX, PROFILE_BLOCK_SIZE, bits);
+		valueUpdater(Profile_Vec, PROFILE_HEADER_SEGMENT_SIZE_INDEX, PROFILE_HEADER_SEGMENT_SIZE, bits);
+		valueUpdater(Profile_Vec, PROFILE_SIZE_INDEX, PROFILE_SIZE, bits);
 
 		File_Vec.swap(Profile_Vec);
 		
-	} else {
+	} else { // Data file is too large for a single profile segment. Create multiple profile segements to store data file.
 
 		std::vector<uint_fast8_t>Profile_Header_Vec = { 0xFF, 0xE2, 0xFF, 0xFF, 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45, 0x00, 0x01, 0x01 };
 		
-		uint_fast8_t profile_count_header_insert_index = 0x0F;
+		uint_fast8_t profile_header_count_insert_index = 0x0F;
 
-		uint_fast16_t profile_count = 1;
+		uint_fast16_t profile_header_count = 1;
 
 		uint_fast32_t 
-			byte_index{},
-			profile_header_insert_tally = PROFILE_VECTOR_SIZE / BLOCK_SIZE,
-			profile_header_total_byte_value = profile_header_insert_tally * PROFILE_HEADER_LENGTH,
-			block_tally = BLOCK_SIZE + JPG_HEADER_LENGTH,
-			last_block_size = (PROFILE_VECTOR_SIZE % BLOCK_SIZE) + (profile_header_total_byte_value - JPG_HEADER_LENGTH) - 2;
+			read_byte_index{},
+			profile_header_count_inserted_tally = PROFILE_WITH_DATA_FILE_VEC_SIZE / SEGMENT_SIZE, // Approx. number of profile segments required. Does not include remainder profile/segments.
+			profile_header_count_total_byte_value = profile_header_count_inserted_tally * PROFILE_HEADER_LENGTH,
+			segment_tally = SEGMENT_SIZE + JPG_HEADER_LENGTH,
+			last_segment_remainder_size = (PROFILE_WITH_DATA_FILE_VEC_SIZE % SEGMENT_SIZE) + (profile_header_count_total_byte_value - PROFILE_HEADER_LENGTH) - 4;
 		
-		while (PROFILE_VECTOR_SIZE > byte_index) {
+		while (PROFILE_WITH_DATA_FILE_VEC_SIZE > read_byte_index) {
 			
-			File_Vec.emplace_back(Profile_Vec[byte_index++]);
+			File_Vec.emplace_back(Profile_Vec[read_byte_index++]);
 
-			if (byte_index == block_tally) {
-				profile_count++;
-				valueUpdater(Profile_Header_Vec, profile_count_header_insert_index, profile_count, bits);
+			if (read_byte_index == segment_tally) { // Another profile segment required.
+				profile_header_count++;
+				valueUpdater(Profile_Header_Vec, profile_header_count_insert_index, profile_header_count, bits);
 
-				File_Vec.insert(File_Vec.begin() + block_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
-				block_tally += BLOCK_SIZE;	
+				File_Vec.insert(File_Vec.begin() + segment_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
+				segment_tally += SEGMENT_SIZE;	
 			}
 		}
 		
-		if (last_block_size > BLOCK_SIZE * 2) {
+		if (last_segment_remainder_size > SEGMENT_SIZE * 2) {
 			
-			last_block_size += (PROFILE_HEADER_LENGTH * 2) - 2;
+			last_segment_remainder_size += (PROFILE_HEADER_LENGTH * 2) - 2;
 						
-			profile_count++;
-			valueUpdater(Profile_Header_Vec, profile_count_header_insert_index, profile_count, bits);
+			profile_header_count++;
+			valueUpdater(Profile_Header_Vec, profile_header_count_insert_index, profile_header_count, bits);
 
-			File_Vec.insert(File_Vec.begin() + block_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
+			File_Vec.insert(File_Vec.begin() + segment_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
 
-			profile_header_insert_tally++;
-			block_tally += BLOCK_SIZE;
+			profile_header_count_inserted_tally++;
+			segment_tally += SEGMENT_SIZE;
 
-			profile_count++;
-			valueUpdater(Profile_Header_Vec, profile_count_header_insert_index, profile_count, bits);		
+			profile_header_count++;
+			valueUpdater(Profile_Header_Vec, profile_header_count_insert_index, profile_header_count, bits);		
 
-			File_Vec.insert(File_Vec.begin() + block_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
-			profile_header_insert_tally++;
+			File_Vec.insert(File_Vec.begin() + segment_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
+			profile_header_count_inserted_tally++;
 
-			valueUpdater(File_Vec, block_tally + 2, last_block_size, bits);	
+			valueUpdater(File_Vec, segment_tally + 2, last_segment_remainder_size, bits);	
 		
-		} else if (last_block_size > BLOCK_SIZE) {
+		} else if (last_segment_remainder_size > SEGMENT_SIZE) {
 			
-			last_block_size += PROFILE_HEADER_LENGTH - 1;
+			last_segment_remainder_size += PROFILE_HEADER_LENGTH - 1;
 			
-			profile_count++;
-			valueUpdater(Profile_Header_Vec, profile_count_header_insert_index, profile_count, bits);
+			profile_header_count++;
+			valueUpdater(Profile_Header_Vec, profile_header_count_insert_index, profile_header_count, bits);
 
-			File_Vec.insert(File_Vec.begin() + block_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
-			profile_header_insert_tally++;
+			File_Vec.insert(File_Vec.begin() + segment_tally, Profile_Header_Vec.begin(), Profile_Header_Vec.end());
+			profile_header_count_inserted_tally++;
 
-			valueUpdater(File_Vec, block_tally + 2, last_block_size, bits);	
+			valueUpdater(File_Vec, segment_tally + 2, last_segment_remainder_size, bits);	
 		
 		} else {  
-
-			block_tally -= BLOCK_SIZE;
-			valueUpdater(File_Vec, block_tally + 2, last_block_size, bits);		
+			
+			segment_tally -= SEGMENT_SIZE;
+			valueUpdater(File_Vec, segment_tally + 2, last_segment_remainder_size, bits);		
 		}
 
-		valueUpdater(File_Vec, PROFILE_TALLY_INDEX, profile_header_insert_tally, bits);
-		profile_header_insert_tally++;
+		valueUpdater(File_Vec, PROFILE_TALLY_INDEX, profile_header_count_inserted_tally, bits);
+		profile_header_count_inserted_tally++;
 
 		constexpr uint_fast8_t	
 			ICC_PROFILE_SIG[] { 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45 },
@@ -105,18 +106,20 @@ void insertProfileHeaders(std::vector<uint_fast8_t>&Profile_Vec, std::vector<uin
 			
 		uint_fast32_t profile_total_insert_index{}; 
 		
-		uint_fast16_t counter = profile_header_insert_tally;
-
+		uint_fast16_t counter = profile_header_count_inserted_tally;
+		
+		// Within the relevant index position for each profile header found within File_Vec, insert the total value of inserted profile headers/segments.
+		// This is a requirement for platforms such as Mastodon. Mastodon has a limit of 100 (0x64) profiles/segments. Which gives it a max storage size of ~6MB.
 		while (counter--) {
 			profile_total_insert_index = searchFunc(File_Vec, profile_total_insert_index, pos_addition, ICC_PROFILE_SIG) + profile_total_insert_index_diff;
-			File_Vec[profile_total_insert_index] = profile_header_insert_tally > profile_tally_update_max ? profile_tally_update_max : profile_header_insert_tally;
+			File_Vec[profile_total_insert_index] = profile_header_count_inserted_tally > profile_tally_update_max ? profile_tally_update_max : profile_header_count_inserted_tally;
 		}	
 		
-		if (profile_header_insert_tally > 100) {
+		if (profile_header_count_inserted_tally > 100) {
 			std::cout << "\n**Warning**\n\nEmbedded image is not compatible with Mastodon. Image file exceeds platform size limit.\n";
 		}   
 	}
 	
 	bits = 32; 
-	valueUpdater(File_Vec, DEFLATED_FILE_SIZE_INDEX, DEFLATED_FILE_SIZE, bits);
+	valueUpdater(File_Vec, DEFLATED_DATA_FILE_SIZE_INDEX, DEFLATED_DATA_FILE_SIZE, bits);
 }
