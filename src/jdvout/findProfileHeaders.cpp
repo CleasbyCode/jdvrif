4961694a2461f7f55a4be0ba7d2325b7871704c1
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-07-28 00:14:11 +0100

    Update findProfileHeaders.cpp

--- src/jdvout/findProfileHeaders.cpp
+++ src/jdvout/findProfileHeaders.cpp
@@ -1,8 +1,6 @@
 void findProfileHeaders(std::vector<uint8_t>&Image_Vec, std::vector<uint32_t>&Profile_Headers_Offset_Vec, uint16_t profile_count) {
 
-	constexpr uint8_t 
-		ICC_PROFILE_SIG[] { 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45},
-		PROFILE_HEADER_LENGTH = 18;
+	constexpr uint8_t ICC_PROFILE_SIG[] { 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45 };
 
 	uint32_t profile_header_index{}; 
 
