7da00d87e9c2d556fd3ff0e853dbfe892481fdd7
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-07-27 22:39:37 +0100

    Update encryptFile.cpp

--- src/jdvin/encryptFile.cpp
+++ src/jdvin/encryptFile.cpp
@@ -1,28 +1,28 @@
-void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_t>& File_Vec, std::string& data_filename) {
+void encryptFile(std::vector<uint8_t>& Profile_Vec, std::vector<uint8_t>& File_Vec, std::string& data_filename) {
 	
 	std::random_device rd;
  	std::mt19937 gen(rd());
 	std::uniform_int_distribution<unsigned short> dis(1, 255); 
 	
-	constexpr uint_fast16_t PROFILE_XOR_KEY_INSERT_INDEX = 0x260;	
+	constexpr uint16_t PROFILE_XOR_KEY_INSERT_INDEX = 0x260;	
 
-	constexpr uint_fast8_t 
+	constexpr uint8_t 
 		XOR_KEY_LENGTH = 12,
 		PROFILE_NAME_INDEX = 0x51;
 
-	const uint_fast32_t DATA_FILE_SIZE = static_cast<uint_fast32_t>(File_Vec.size());
+	const uint32_t DATA_FILE_SIZE = static_cast<uint32_t>(File_Vec.size());
 
-	const uint_fast8_t DATA_FILENAME_LENGTH = static_cast<uint_fast8_t>(data_filename.length());
+	const uint8_t DATA_FILENAME_LENGTH = static_cast<uint8_t>(data_filename.length());
        
-	uint_fast32_t index_pos{};
+	uint32_t index_pos{};
 
-	uint_fast8_t 
+	uint8_t 
 		xor_key[XOR_KEY_LENGTH],
 		xor_key_pos{},
 		name_key_pos{};
 	
 	for (int i = 0; i < XOR_KEY_LENGTH; ++i) {
-        	xor_key[i] = static_cast<uint_fast8_t>(dis(gen));
+        	xor_key[i] = static_cast<uint8_t>(dis(gen));
     	}
 	
 	std::copy(std::begin(xor_key), std::end(xor_key), Profile_Vec.begin() + PROFILE_XOR_KEY_INSERT_INDEX);
