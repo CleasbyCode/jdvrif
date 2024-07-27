45cef37b1d57df114f1d3bf9f855c0ead6fc5c6c
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-07-27 22:59:17 +0100

    Update searchFunc.cpp

--- src/jdvin/searchFunc.cpp
+++ src/jdvin/searchFunc.cpp
@@ -1,5 +1,5 @@
-template <uint_fast8_t N>
-uint_fast32_t searchFunc(std::vector<uint_fast8_t>& Vec, uint_fast32_t val_a, uint_fast8_t val_b, const uint_fast8_t (&SIG)[N]) {
-	uint_fast32_t SIG_POS = static_cast<uint_fast32_t>(std::search(Vec.begin() + val_a + val_b, Vec.end(), std::begin(SIG), std::end(SIG)) - Vec.begin());
+template <uint8_t N>
+uint32_t searchFunc(std::vector<uint8_t>& Vec, uint32_t val_a, uint8_t val_b, const uint8_t (&SIG)[N]) {
+	uint32_t SIG_POS = static_cast<uint32_t>(std::search(Vec.begin() + val_a + val_b, Vec.end(), std::begin(SIG), std::end(SIG)) - Vec.begin());
 	return SIG_POS;
 }
