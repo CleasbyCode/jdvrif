70f7f9a482c1e1b805e24e9f562638ab03a0c411
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-08-19 18:55:59 +0100

    Delete src/jdvout directory

--- src/jdvout/fourBytes.cpp
+++
@@ -1,6 +0,0 @@
-uint32_t getFourByteValue(const std::vector<uint8_t>& VEC, const uint32_t INDEX) {
-	return	(static_cast<uint32_t>(VEC[INDEX]) << 24) |
-		(static_cast<uint32_t>(VEC[INDEX + 1]) << 16) |
-		(static_cast<uint32_t>(VEC[INDEX + 2]) << 8) |
-		 static_cast<uint32_t>(VEC[INDEX + 3]); 
-}
