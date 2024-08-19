cecc74b3fa82e40674d1e945a7327910ef65e03d
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-08-19 18:50:07 +0100

    Delete src/jdvin directory

--- src/jdvin/searchFunc.cpp
+++
@@ -1,5 +0,0 @@
-template <uint8_t N>
-uint32_t searchFunc(std::vector<uint8_t>& Vec, uint32_t val_a, uint8_t val_b, const uint8_t (&SIG)[N]) {
-	uint32_t SIG_POS = static_cast<uint32_t>(std::search(Vec.begin() + val_a + val_b, Vec.end(), std::begin(SIG), std::end(SIG)) - Vec.begin());
-	return SIG_POS;
-}
--- src/jdvin/valueUpdater.cpp
+++
@@ -1,5 +0,0 @@
-void valueUpdater(std::vector<uint8_t>& vec, uint32_t value_insert_index, const uint32_t VALUE, uint8_t bits) {
-	while (bits) {
-		static_cast<uint32_t>(vec[value_insert_index++] = (VALUE >> (bits -= 8)) & 0xff);
-	}
-}
