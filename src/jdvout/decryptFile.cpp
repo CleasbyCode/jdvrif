02de07b4b2b309419fe9a857c644be07f0be73ff
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-08-27 10:08:37 +0100

    Add files via upload

---
+++ src/jdvout/decryptFile.cpp
@@ -0,0 +1,47 @@
+std::string decryptFile(std::vector<uint_fast8_t>&Image_Vec, std::vector<uint_fast32_t>&Profile_Headers_Offset_Vec, std::string& encrypted_data_filename) {
+
+	constexpr uint_fast8_t
+		XOR_KEY_LENGTH = 12,		
+		PROFILE_HEADER_LENGTH = 18;
+
+	const uint_fast32_t PROFILE_VEC_SIZE = static_cast<uint_fast32_t>(Profile_Headers_Offset_Vec.size());
+
+	const uint_fast8_t ENCRYPTED_DATA_FILENAME_LENGTH = static_cast<uint_fast8_t>(encrypted_data_filename.length() - XOR_KEY_LENGTH);
+
+	uint_fast32_t
+		encrypted_data_file_size = static_cast<uint_fast32_t>(Image_Vec.size()),
+		offset_index{},
+		decrypt_pos{},
+		index_pos{};
+
+	uint_fast8_t
+		xor_key[XOR_KEY_LENGTH],
+		xor_key_pos{},
+		name_key_pos{};
+
+	for (uint_fast8_t j = 0, i = ENCRYPTED_DATA_FILENAME_LENGTH; i < ENCRYPTED_DATA_FILENAME_LENGTH + XOR_KEY_LENGTH;) {
+        	xor_key[j++] = encrypted_data_filename[i++]; 
+    	}
+
+	std::string decrypted_data_filename;
+
+	while (encrypted_data_file_size > index_pos) {
+		if (index_pos >= ENCRYPTED_DATA_FILENAME_LENGTH) {
+			name_key_pos = name_key_pos >= ENCRYPTED_DATA_FILENAME_LENGTH ? 0 : name_key_pos;
+		} else {
+			std::reverse(std::begin(xor_key), std::end(xor_key));
+
+			xor_key_pos = xor_key_pos >= XOR_KEY_LENGTH ? 0 : xor_key_pos;
+			decrypted_data_filename += encrypted_data_filename[index_pos] ^ xor_key[xor_key_pos++];
+		}
+
+		Image_Vec[decrypt_pos++] = Image_Vec[index_pos++] ^ encrypted_data_filename[name_key_pos++];
+
+		if (PROFILE_VEC_SIZE && index_pos == Profile_Headers_Offset_Vec[offset_index]) {
+			index_pos += PROFILE_HEADER_LENGTH; 
+			encrypted_data_file_size += PROFILE_HEADER_LENGTH; 
+			offset_index++;	
+		}	
+	}
+	return decrypted_data_filename;
+}
---
+++ src/jdvout/main.cpp
@@ -0,0 +1,37 @@
+//	JPG Data Vehicle (jdvout v1.1.9) Created by Nicholas Cleasby (@CleasbyCode) 10/04/2023
+//
+//	To compile program (Linux):
+// 	$ g++ main.cpp -O2 -lz -s -o jdvout
+
+// 	Run it:
+// 	$ ./jdvout
+
+#include "jdvout.h"
+
+int main(int argc, char** argv) {
+	if (argc !=2) {
+		std::cout << "\nUsage: jdvout <file_embedded_image>\n\t\bjdvout --info\n\n";
+	} else if (std::string(argv[1]) == "--info") {
+		displayInfo();
+	} else {
+		const std::string IMAGE_FILENAME = std::string(argv[1]);
+
+		const std::regex REG_EXP("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+
+		std::string file_extension = IMAGE_FILENAME.length() > 3 ? IMAGE_FILENAME.substr(IMAGE_FILENAME.length() - 4) : IMAGE_FILENAME;
+
+		file_extension = file_extension == "jpeg" || file_extension == "jfif" ? ".jpg" : file_extension;
+
+		if (file_extension == ".jpg" && regex_match(IMAGE_FILENAME, REG_EXP) && std::filesystem::exists(IMAGE_FILENAME)) {
+			jdvOut(IMAGE_FILENAME);
+		} else {
+			std::cerr << (file_extension != ".jpg" 
+				? "\nFile Type Error: Invalid file extension found. Expecting only \"jpg\""
+				: !regex_match(IMAGE_FILENAME, REG_EXP)
+					? "\nInvalid Input Error: Characters not supported by this program found within filename arguments"
+						: "\nImage File Error: File not found. Check the filename and try again")
+			 << ".\n\n";
+			return 1;
+		}
+	} 
+}
