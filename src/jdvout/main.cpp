5fd3fd5f2401b83ea96f6d29c12dff489518e54a
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-14 15:48:43 +0100

    Update main.cpp

--- src/jdvout/main.cpp
+++ src/jdvout/main.cpp
@@ -9,39 +9,37 @@
 #include "jdvout.h"
 
 int main(int argc, char** argv) {
-    if (argc != 2) {
-        std::cout << "\nUsage: jdvout <file_embedded_image>\n\t\bjdvout --info\n\n";
-        return 1;
-    }
+	if (argc != 2) {
+		std::cout << "\nUsage: jdvout <file_embedded_image>\n\t\bjdvout --info\n\n";
+        	return 1;
+    	}
 	
-    if (std::string(argv[1]) == "--info") {
-        displayInfo();
-        return 0;
-    }
+	if (std::string(argv[1]) == "--info") {
+		displayInfo();
+        	return 0;
+    	}
 
-    const std::string IMAGE_FILENAME = std::string(argv[1]);
+    	const std::string IMAGE_FILENAME = std::string(argv[1]);
 
-    std::filesystem::path image_path(IMAGE_FILENAME);
-    std::string image_extension = image_path.extension().string();
+    	constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+    	const std::regex regex_pattern(REG_EXP);
 
-    image_extension = image_extension == ".jpeg" || image_extension == ".jfif" ? ".jpg" : image_extension;
+    	if (!std::regex_match(IMAGE_FILENAME, regex_pattern)) {
+		std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
+        	return 1;
+    	}
+	
+	std::filesystem::path image_path(IMAGE_FILENAME);
+    	const std::string IMAGE_EXTENSION = image_path.extension().string();
 
-    if (image_extension != ".jpg") {
-        std::cerr << "\nFile Type Error: Invalid file extension. Expecting only \".jpg\" image extension.\n\n";
-        return 1;
-    }
+    	if (IMAGE_EXTENSION != ".jpg" && IMAGE_EXTENSION != ".jpeg" && IMAGE_EXTENSION != ".jfif") {
+		std::cerr << "\nFile Type Error: Invalid file extension. Expecting only \"jpg, jpeg or jfif\" image extensions.\n\n";
+        	return 1;
+	}
 
-    constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
-    const std::regex regex_pattern(REG_EXP);
-
-    if (!std::regex_match(IMAGE_FILENAME, regex_pattern)) {
-        std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
-        return 1;
-    }
-
-    if (!std::filesystem::exists(IMAGE_FILENAME) || !std::filesystem::is_regular_file(IMAGE_FILENAME)) {
-        std::cerr << "\nImage File Error: File not found or not a regular file. Check the filename and try again.\n\n";
-        return 1;
-    }
-    jdvOut(IMAGE_FILENAME);
+    	if (!std::filesystem::exists(IMAGE_FILENAME)) {
+		std::cerr << "\nImage File Error: File not found. Check the filename and try again.\n\n";
+        	return 1;
+    	}
+	jdvOut(IMAGE_FILENAME);
 }
