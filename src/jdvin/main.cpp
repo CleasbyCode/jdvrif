2cc1d8a17a7220b65f056e3cf605f966193c3461
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-14 16:34:13 +0100

    Update main.cpp

--- src/jdvin/main.cpp
+++ src/jdvin/main.cpp
@@ -10,26 +10,34 @@
 
 int main(int argc, char** argv) {
 	if (argc == 2 && std::string(argv[1]) == "--info") {
-		displayInfo();
+        	displayInfo();
         	return 0;
-	}
-	
-	if (argc < 3 || argc > 4) {
+    	}
+
+    	if (argc < 3 || argc > 4) {
 		std::cout << "\nUsage: jdvin [-r] <cover_image> <data_file>\n\t\bjdvin --info\n\n";
         	return 1;
-	}
+    	}
    
-	const bool 
+    	const bool 
 		isRedditOption = (argc > 3 && std::string(argv[1]) == "-r"),
-		isInvalidOption = (argc > 3 && !isRedditOption);
+	    	isInvalidOption = (argc > 3 && !isRedditOption);
 
     	if (isInvalidOption) {
 		std::cerr << "\nInput Error: Invalid arguments. Expecting only \"-r\" as the first optional argument.\n\n";
-        	return 1;
-    	}
-
+		return 1;
+	}
+	
     	const std::string IMAGE_FILENAME = isRedditOption ? argv[2] : argv[1];
     	std::string data_filename = isRedditOption ? argv[3] : argv[2];
+
+    	constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+    	const std::regex regex_pattern(REG_EXP);
+
+    	if (!std::regex_match(IMAGE_FILENAME, regex_pattern) || !std::regex_match(data_filename, regex_pattern)) {
+		std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
+        	return 1;
+    	}
 
     	const std::filesystem::path image_path(IMAGE_FILENAME);
     	const std::string IMAGE_EXTENSION = image_path.extension().string();
@@ -39,20 +47,12 @@ int main(int argc, char** argv) {
         	return 1;
 	}
 
-    	constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
-    	const std::regex regex_pattern(REG_EXP);
-
-    	if (!std::regex_match(IMAGE_FILENAME, regex_pattern) || !std::regex_match(data_filename, regex_pattern)) {
-		std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
-	    	return 1;
-    	}
-
     	if (!std::filesystem::exists(IMAGE_FILENAME) || !std::filesystem::exists(data_filename) || !std::filesystem::is_regular_file(data_filename)) {
 		std::cerr << (!std::filesystem::exists(IMAGE_FILENAME)
-            		? "\nImage File Error: File not found."
-            		: "\nData File Error: File not found or not a regular file.")
+			? "\nImage File Error: File not found."
+			: "\nData File Error: File not found or not a regular file.")
             	<< " Check the filename and try again.\n\n";
-	    	return 1;
+        	return 1;
 	}
 	jdvIn(IMAGE_FILENAME, data_filename, isRedditOption);
 }
