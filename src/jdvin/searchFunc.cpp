0ad6c6578fe693ba024a932156a18770faf0dcc8
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-18 03:04:14 +0100

    Delete src/jdvin directory

--- src/jdvin/searchFunc.cpp
+++
@@ -1,6 +0,0 @@
-// Search and return a vector index position of given string signature.
-template <uint_fast8_t N>
-uint_fast32_t searchFunc(std::vector<uint_fast8_t>& Vec, uint_fast32_t start_index, const uint_fast8_t INCREMENT_SEARCH_INDEX, const uint_fast8_t (&SIG)[N]) {
-	uint_fast32_t SIG_POS = static_cast<uint_fast32_t>(std::search(Vec.begin() + start_index + INCREMENT_SEARCH_INDEX, Vec.end(), std::begin(SIG), std::end(SIG)) - Vec.begin());
-	return SIG_POS;
-}
