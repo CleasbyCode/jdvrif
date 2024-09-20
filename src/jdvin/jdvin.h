21fbc3ad79240f8578bdfb7149555a191322b1c3
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-20 22:24:47 +0100

    Update jdvin.h

--- src/jdvin/jdvin.h
+++ src/jdvin/jdvin.h
@@ -25,17 +25,17 @@
 #include "jdvin.cpp"
 
 template <uint_fast8_t N>
-uint_fast32_t searchFunc(std::vector<uint_fast8_t>&, uint_fast32_t, const uint_fast8_t, const uint_fast8_t (&)[N]);
+uint_fast32_t searchFunc(std::vector<uint8_t>&, uint_fast32_t, const uint_fast8_t, const uint_fast8_t (&)[N]);
 
-uint_fast32_t deflateFile(std::vector<uint_fast8_t>&);
+uint_fast32_t deflateFile(std::vector<uint8_t>&);
 
-bool writeFile(std::vector<uint_fast8_t>&);
+bool writeFile(std::vector<uint8_t>&);
 
 void
-	eraseSegments(std::vector<uint_fast8_t>&, bool&),
-	encryptFile(std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&, uint_fast32_t, std::string&),
-	insertProfileHeaders(std::vector<uint_fast8_t>&, std::vector<uint_fast8_t>&),
-	valueUpdater(std::vector<uint_fast8_t>&, uint_fast32_t, const uint_fast32_t, uint_fast8_t),
+	eraseSegments(std::vector<uint8_t>&, bool&),
+	encryptFile(std::vector<uint8_t>&, std::vector<uint8_t>&, uint_fast32_t, std::string&),
+	insertProfileHeaders(std::vector<uint8_t>&, std::vector<uint8_t>&),
+	valueUpdater(std::vector<uint8_t>&, uint_fast32_t, const uint_fast32_t, uint_fast8_t),
 	displayInfo();
 
 uint_fast8_t jdvIn(const std::string&, std::string&, bool);
