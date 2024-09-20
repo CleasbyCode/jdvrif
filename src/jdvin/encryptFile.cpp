d7e52cdca7b9efa41dca5fb31658382350d6e636
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-20 22:20:39 +0100

    Update encryptFile.cpp

--- src/jdvin/encryptFile.cpp
+++ src/jdvin/encryptFile.cpp
@@ -1,4 +1,4 @@
-void encryptFile(std::vector<uint_fast8_t>& Profile_Vec, std::vector<uint_fast8_t>& File_Vec, uint_fast32_t data_file_size, std::string& data_filename) {
+void encryptFile(std::vector<uint8_t>& Profile_Vec, std::vector<uint8_t>& File_Vec, uint_fast32_t data_file_size, std::string& data_filename) {
 	
 	std::random_device rd;
  	std::mt19937 gen(rd());
