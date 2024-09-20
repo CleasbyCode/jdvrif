420ac47a0b8b02acebd54b9340c221c3e8d656bb
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-20 22:36:17 +0100

    Update jdvout.h

--- src/jdvout/jdvout.h
+++ src/jdvout/jdvout.h
@@ -19,16 +19,16 @@
 #include "information.cpp"
 #include "jdvout.cpp"
 
-std::string decryptFile(std::vector<uint_fast8_t>&, std::vector<uint_fast32_t>&, std::string&, const uint_fast8_t);
+std::string decryptFile(std::vector<uint8_t>&, std::vector<uint_fast32_t>&, std::string&, const uint_fast8_t);
 
 template <uint_fast8_t N>
-uint_fast32_t searchFunc(std::vector<uint_fast8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
+uint_fast32_t searchFunc(std::vector<uint8_t>&, uint_fast32_t, uint_fast8_t, const uint_fast8_t (&)[N]);
 
-uint_fast32_t getByteValue(const std::vector<uint_fast8_t>&, const uint_fast32_t);
+uint_fast32_t getByteValue(const std::vector<uint8_t>&, const uint_fast32_t);
 
 void
-	findProfileHeaders(std::vector<uint_fast8_t>&, std::vector<uint_fast32_t>&, uint_fast16_t),
-	inflateFile(std::vector<uint_fast8_t>&),
+	findProfileHeaders(std::vector<uint8_t>&, std::vector<uint_fast32_t>&, uint_fast16_t),
+	inflateFile(std::vector<uint8_t>&),
 	displayInfo();
 
 uint_fast8_t jdvOut(const std::string&);	
