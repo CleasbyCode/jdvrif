8e73714feb19bed2b8e686e963ce7582fce8aa11
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-09-20 22:32:33 +0100

    Update findProfileHeaders.cpp

--- src/jdvout/findProfileHeaders.cpp
+++ src/jdvout/findProfileHeaders.cpp
@@ -1,7 +1,6 @@
 // Search the "file-embedded" image for ICC Profile headers. Store index location of each found header within a vector.
 // We will use these index positions to skip over the headers when decrypting the data file, so that they are not included within the restored data file.
-void findProfileHeaders(std::vector<uint_fast8_t>&Image_Vec, std::vector<uint_fast32_t>&Profile_Headers_Index_Vec, uint_fast16_t profile_count) {
-
+void findProfileHeaders(std::vector<uint8_t>&Image_Vec, std::vector<uint_fast32_t>&Profile_Headers_Index_Vec, uint_fast16_t profile_count) {
 	constexpr uint_fast8_t 
 		ICC_PROFILE_SIG[] { 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45 },
 		NEXT_SEARCH_POS_INC 	= 5,
