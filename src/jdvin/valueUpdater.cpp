c6c27493597c904a4e53f7757188c53757542f44
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-03 14:07:52 +0100

    Delete src/jdvin directory

--- src/jdvin/valueUpdater.cpp
+++
@@ -1,6 +0,0 @@
-// Writes updated values, such as segment size lengths and other values, into the relevant vector index locations.
-void valueUpdater(std::vector<uint8_t>& vec, uint_fast32_t value_insert_index, const uint_fast32_t VALUE, uint_fast8_t bits) {
-	while (bits) {
-		vec[value_insert_index++] = (VALUE >> (bits -= 8)) & 0xff;
-	}
-}
