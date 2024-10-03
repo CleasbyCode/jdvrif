5c6e72d8e4f7e2c99d605f7d58348fbb84229af3
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-03 14:29:31 +0100

    Update main.cpp

--- src/jdvout/main.cpp
+++ src/jdvout/main.cpp
@@ -2,9 +2,9 @@
 //
 //	To compile program (Linux):
 // 	$ g++ main.cpp -O2 -lz -s -o jdvout
-
+//	$ sudo cp jdvout /usr/bin
 // 	Run it:
-// 	$ ./jdvout
+// 	$ jdvout
 
 #include "jdvout.h"
 
@@ -16,30 +16,30 @@ int main(int argc, char** argv) {
 	
 	if (std::string(argv[1]) == "--info") {
 		displayInfo();
-        	return 0;
-    	}
+		return 0;
+	}
 
-    const std::string IMAGE_FILENAME = std::string(argv[1]);
+    	const std::string IMAGE_FILENAME = std::string(argv[1]);
 
-    constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
-    const std::regex regex_pattern(REG_EXP);
+    	constexpr const char* REG_EXP = ("(\\.[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+)?[a-zA-Z_0-9\\.\\\\\\s\\-\\/]+?(\\.[a-zA-Z0-9]+)?");
+    	const std::regex regex_pattern(REG_EXP);
 
-    if (!std::regex_match(IMAGE_FILENAME, regex_pattern)) {
+    	if (!std::regex_match(IMAGE_FILENAME, regex_pattern)) {
 		std::cerr << "\nInvalid Input Error: Characters not supported by this program found within filename arguments.\n\n";
-        	return 1;
+	    	return 1;
     	}
 	
-    const std::filesystem::path IMAGE_PATH(IMAGE_FILENAME);
-    const std::string IMAGE_EXTENSION = IMAGE_PATH.extension().string();
+    	const std::filesystem::path IMAGE_PATH(IMAGE_FILENAME);
+    	const std::string IMAGE_EXTENSION = IMAGE_PATH.extension().string();
 
-    if (IMAGE_EXTENSION != ".jpg" && IMAGE_EXTENSION != ".jpeg" && IMAGE_EXTENSION != ".jfif") {
-	std::cerr << "\nFile Type Error: Invalid file extension. Expecting only \"jpg, jpeg or jfif\" image extensions.\n\n";
-        return 1;
-    }
+    	if (IMAGE_EXTENSION != ".jpg" && IMAGE_EXTENSION != ".jpeg" && IMAGE_EXTENSION != ".jfif") {
+	    	std::cerr << "\nFile Type Error: Invalid file extension. Expecting only \"jpg, jpeg or jfif\" image extensions.\n\n";
+	    	return 1;
+    	}
 
     	if (!std::filesystem::exists(IMAGE_FILENAME)) {
-		std::cerr << "\nImage File Error: File not found. Check the filename and try again.\n\n";
-        	return 1;
+	    	std::cerr << "\nImage File Error: File not found. Check the filename and try again.\n\n";
+	    	return 1;
     	}
 	jdvOut(IMAGE_FILENAME);
 }
