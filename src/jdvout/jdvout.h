d6e3f8adb04810e0b520c454e81976c89e176cc5
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-04 16:13:25 +0100

    Update jdvout.h

--- src/jdvout/jdvout.h
+++ src/jdvout/jdvout.h
@@ -19,7 +19,7 @@
 #include "information.cpp"
 #include "jdvout.cpp"
 
-std::string decryptFile(std::vector<uint8_t>&, const uint32_t*, const uint8_t*, const uint16_t, std::string&);
+std::string decryptFile(std::vector<uint8_t>&, const uint32_t*, const uint8_t*, const uint16_t, const std::string&);
 
 template <uint8_t N>
 uint32_t searchFunc(std::vector<uint8_t>&, uint32_t, uint8_t, const uint8_t (&SIG)[N]);
