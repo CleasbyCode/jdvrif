38b15fb05eb0322d5cdb32da6caf13b299cfccbf
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-24 00:23:37 +0100

    Update segmentDataFile.cpp

--- src/jdvin/segmentDataFile.cpp
+++ src/jdvin/segmentDataFile.cpp
@@ -10,13 +10,15 @@ void segmentDataFile(std::vector<uint8_t>&Profile_Vec, std::vector<uint8_t>&File
 	const uint32_t COLOR_PROFILE_WITH_DATA_FILE_VEC_SIZE = static_cast<uint32_t>(Profile_Vec.size());
 
 	uint32_t segment_data_size = 65519;
+
 	uint8_t value_bit_length = 16;	
 
 	if (segment_data_size + JPG_HEADER_LENGTH + SEGMENT_HEADER_LENGTH >= COLOR_PROFILE_WITH_DATA_FILE_VEC_SIZE) { 
 		// Data file is small enough to fit all within the first/main APP2 segment, appended to the color profile data.
 		constexpr uint8_t
-			SEGMENT_HEADER_SIZE_INDEX = 0x16, // Two byte JPG APP2 profile header segment size field index.
-			COLOR_PROFILE_SIZE_INDEX  = 0x28, // Four byte ICC profile size field index.
+			SEGMENT_HEADER_SIZE_INDEX = 0x16, // Start index of the two byte JPG APP2 profile header segment size field.
+			SEGMENT_TOTAL_VALUE_INDEX = 0x25,
+			COLOR_PROFILE_SIZE_INDEX  = 0x28, // Start index of the four byte ICC profile size field index.
 			COLOR_PROFILE_SIZE_DIFF   = 16;	  // Size difference between SEGMENT_SIZE & COLOR_PROFILE_SIZE.
 		
 		const uint32_t 
@@ -25,68 +27,73 @@ void segmentDataFile(std::vector<uint8_t>&Profile_Vec, std::vector<uint8_t>&File
 
 		valueUpdater(Profile_Vec, SEGMENT_HEADER_SIZE_INDEX, SEGMENT_SIZE, value_bit_length);
 		valueUpdater(Profile_Vec, COLOR_PROFILE_SIZE_INDEX, COLOR_PROFILE_SIZE, value_bit_length);
-		Profile_Vec[0x24] = Profile_Vec[0x25] = 1; // Change the default segment sequence and segment total values to 1. For X/Twitter compatibility.
+		Profile_Vec[SEGMENT_TOTAL_VALUE_INDEX] = 1; // For X/Twitter compatibility.
 		File_Vec = std::move(Profile_Vec);
 	} else { 
 		// Data file is too large for the first APP2 profile segment. Create additional segments as needed, to store the data file.
 		uint32_t 
 			segments_required_approx_val = COLOR_PROFILE_WITH_DATA_FILE_VEC_SIZE / segment_data_size,
-			segment_remainder_size	     = COLOR_PROFILE_WITH_DATA_FILE_VEC_SIZE % segment_data_size,
 			byte_index 		     = 0;
-		
+
+		const uint32_t 
+			SEGMENT_REMAINDER_SIZE = COLOR_PROFILE_WITH_DATA_FILE_VEC_SIZE % segment_data_size,
+			FIRST_SEGMENT_DATA_SIZE = segment_data_size + JPG_HEADER_LENGTH + SEGMENT_HEADER_LENGTH;
+
 		constexpr uint8_t 
-			SEGMENT_HEADER[SEGMENT_HEADER_LENGTH] { 0xFF, 0xE2, 0xFF, 0xFF, 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45, 0x00, 0x00, 0xFF },
-			SEGMENTS_TOTAL_VAL_INDEX = 0x8A;  // Index location within color profile data area, to store total value of APP2 profile segments (-1). For jdvout.
+			SEGMENTS_TOTAL_VAL_INDEX = 0x8A,  // Index location within color profile data area, to store total value of APP2 profile segments (-1). For jdvout.
+			SEGMENT_REMAINDER_DIFF = 16;
 		
 		// Write total number of APP2 profile segments (minus the first one) within the index position of the first profile segment. For jdvout.
 		valueUpdater(Profile_Vec, SEGMENTS_TOTAL_VAL_INDEX, segments_required_approx_val, value_bit_length);
 
 		// Create vector of vectors to store the individual segment vectors.
 		std::vector<std::vector<uint8_t>> Segments_Arr_Vec;
+	
+		segment_data_size = FIRST_SEGMENT_DATA_SIZE;
 
-		segment_data_size += JPG_HEADER_LENGTH + SEGMENT_HEADER_LENGTH; // Just for the first segment.
+		uint8_t 
+			segments_sequence_value_index = 0x0F,
+			segment_remainder_size_index = 2;
 
-		uint8_t segments_sequence_value_index = 0x0F;
 		uint16_t segments_sequence_value = 1;
 
-		std::vector<uint8_t> Segment_Vec;
-		Segment_Vec.reserve(segment_data_size + segment_remainder_size);
+		std::vector<uint8_t> Segment_Vec { 0xFF, 0xE2, 0xFF, 0xFF, 0x49, 0x43, 0x43, 0x5F, 0x50, 0x52, 0x4F, 0x46, 0x49, 0x4C, 0x45, 0x00, 0x00, 0xFF };
+		Segment_Vec.reserve(segment_data_size + SEGMENT_REMAINDER_SIZE);
 
-		while (segments_required_approx_val--) {	
-			if (byte_index) {
-				std::copy(std::begin(SEGMENT_HEADER), std::end(SEGMENT_HEADER), std::back_inserter(Segment_Vec));
+		if (SEGMENT_REMAINDER_SIZE) {
+    			segments_required_approx_val++;
+		}	
+		
+		while (segments_required_approx_val--) {
+			if (!segments_required_approx_val && SEGMENT_REMAINDER_SIZE) {
+				segment_data_size = SEGMENT_REMAINDER_SIZE;		
+			   	valueUpdater(Segment_Vec, segment_remainder_size_index, SEGMENT_REMAINDER_SIZE + SEGMENT_REMAINDER_DIFF, value_bit_length); 		
 			}
-			valueUpdater(Segment_Vec, segments_sequence_value_index, segments_sequence_value, value_bit_length);
-			while (segment_data_size--) {
-				Segment_Vec.emplace_back(Profile_Vec[byte_index++]);
-			}
-    			Segments_Arr_Vec.emplace_back(Segment_Vec);
+
+			std::copy_n(Profile_Vec.begin() + byte_index, segment_data_size, std::back_inserter(Segment_Vec));
+			byte_index += segment_data_size;	
+			
+			if (segment_data_size == FIRST_SEGMENT_DATA_SIZE) {
+				Segments_Arr_Vec.emplace_back(Segment_Vec.begin() + SEGMENT_HEADER_LENGTH, Segment_Vec.end());
+			} else {
+        			Segments_Arr_Vec.emplace_back(Segment_Vec);
+    			}
+
+    			Segment_Vec.erase(Segment_Vec.begin() + SEGMENT_HEADER_LENGTH, Segment_Vec.end());
 			segment_data_size = 65519;
-			segments_sequence_value++;
-			Segment_Vec.clear();	
+			
+			valueUpdater(Segment_Vec, segments_sequence_value_index, ++segments_sequence_value, value_bit_length);
 		}
-		if (segment_remainder_size) {
-			constexpr uint8_t SEGMENT_REMAINDER_DIFF = 16;
-			uint8_t segment_remainder_size_index = 2;
-			std::copy(std::begin(SEGMENT_HEADER), std::end(SEGMENT_HEADER), std::back_inserter(Segment_Vec));			
-
-			valueUpdater(Segment_Vec, segment_remainder_size_index, segment_remainder_size + SEGMENT_REMAINDER_DIFF, value_bit_length);
-			valueUpdater(Segment_Vec, segments_sequence_value_index, segments_sequence_value, value_bit_length);
-			while (segment_remainder_size--) {
-				Segment_Vec.emplace_back(Profile_Vec[byte_index++]);	
-			}
-			Segments_Arr_Vec.emplace_back(Segment_Vec);
-		}
-
+		
 		std::vector<uint8_t>().swap(Profile_Vec);
 		File_Vec.reserve(segment_data_size * segments_sequence_value);
 		for (auto& vec : Segments_Arr_Vec) {
-        		File_Vec.insert(File_Vec.end(), vec.begin(), vec.end());
+			File_Vec.insert(File_Vec.end(), vec.begin(), vec.end());
 			std::vector<uint8_t>().swap(vec);
     		}
 		std::vector<std::vector<uint8_t>>().swap(Segments_Arr_Vec);
 		
-		constexpr uint8_t MASTODON_SEGMENTS_LIMIT = 100;  
+		constexpr uint8_t MASTODON_SEGMENTS_LIMIT = 100;		   
 		constexpr uint32_t MASTODON_IMAGE_UPLOAD_LIMIT = 16777216;
 					   
 		if (segments_sequence_value > MASTODON_SEGMENTS_LIMIT && MASTODON_IMAGE_UPLOAD_LIMIT > COLOR_PROFILE_WITH_DATA_FILE_VEC_SIZE) {
