5c0100e7dc69a3bc42006edfd6afe4be3c256b3b
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-25 02:55:57 +0100

    Update writeFile.cpp

--- src/jdvin/writeFile.cpp
+++ src/jdvin/writeFile.cpp
@@ -1,23 +1,23 @@
 bool writeFile(std::vector<uint8_t>& Vec) {
-	srand((unsigned)time(NULL));  
+	std::random_device rd;
+    	std::mt19937 gen(rd());
+    	std::uniform_int_distribution<> dist(10000, 99999);  // Five-digit random number
 
-	const std::string 
-		TIME_VALUE 		= std::to_string(rand()),
-		EMBEDDED_IMAGE_FILENAME = "jrif_" + TIME_VALUE.substr(0, 5) + ".jpg";
+	const std::string IMAGE_FILENAME = "jrif_" + std::to_string(dist(gen)) + ".jpg";
 
-	std::ofstream file_ofs(EMBEDDED_IMAGE_FILENAME, std::ios::binary);
+	std::ofstream file_ofs(IMAGE_FILENAME, std::ios::binary);
 
 	if (!file_ofs) {
 		std::cerr << "\nWrite Error: Unable to write to file.\n\n";
 		return false;
 	}
 	
-	const uint32_t EMBEDDED_IMAGE_SIZE = static_cast<uint32_t>(Vec.size());
+	const uint32_t IMAGE_SIZE = static_cast<uint32_t>(Vec.size());
 
-	file_ofs.write((char*)&Vec[0], EMBEDDED_IMAGE_SIZE);
+	file_ofs.write(reinterpret_cast<const char*>(Vec.data()), IMAGE_SIZE);
 	
 	std::vector<uint8_t>().swap(Vec);
 	
-	std::cout << "\nSaved \"file-embedded\" JPG image: " + EMBEDDED_IMAGE_FILENAME + '\x20' + std::to_string(EMBEDDED_IMAGE_SIZE) + " Bytes.\n";
+	std::cout << "\nSaved \"file-embedded\" JPG image: " << IMAGE_FILENAME << " (" << IMAGE_SIZE << " bytes).\n";
 	return true;
 }
