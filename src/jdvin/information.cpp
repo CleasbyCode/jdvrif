a2d92e19b99aff0d74da41ab9c58d5fed399eaf4
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-11-13 19:26:22 +0000

    Update information.cpp

--- src/jdvin/information.cpp
+++ src/jdvin/information.cpp
@@ -4,7 +4,7 @@ void displayInfo() {
 JPG Data Vehicle (jdvin v2.2). 
 Created by Nicholas Cleasby (@CleasbyCode) 24/01/2023.
 
-jdvin is a steganography-like CLI tool for embedding & concealing any file type within a JPG image. 
+jdvin is a data-hiding / steganography-like CLI tool for concealing any file type within a JPG image. 
 
 Compile & run jdvin (Linux):
 		
