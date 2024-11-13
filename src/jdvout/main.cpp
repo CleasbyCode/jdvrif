2b8b76ed0f341d865e5ab1ac7c00d8c36352d5f8
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-11-13 18:07:59 +0000

    Update main.cpp

--- src/jdvout/main.cpp
+++ src/jdvout/main.cpp
@@ -1,4 +1,4 @@
-//	JPG Data Vehicle (jdvout v2.1) Created by Nicholas Cleasby (@CleasbyCode) 10/04/2023
+//	JPG Data Vehicle (jdvout v2.2) Created by Nicholas Cleasby (@CleasbyCode) 10/04/2023
 //
 //	To compile program (Linux):
 // 	$ g++ main.cpp -O2 -lz -s -o jdvout
