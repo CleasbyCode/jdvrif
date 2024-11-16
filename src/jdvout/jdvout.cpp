2fc4ae8a4b1addf8d59a5b3c8c65ecf210466b64
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-11-16 17:45:34 +0000

    Update jdvout.cpp

--- src/jdvout/jdvout.cpp
+++ src/jdvout/jdvout.cpp
@@ -24,18 +24,22 @@ int jdvOut(const std::string& IMAGE_FILENAME) {
 	constexpr uint8_t
 		JDV_SIG[]	{ 0xB4, 0x6A, 0x3E, 0xEA, 0x5E, 0x9D, 0xF9 },
 		PROFILE_SIG[] 	{ 0x6D, 0x6E, 0x74, 0x72, 0x52, 0x47, 0x42 },
+		DATA_FILE_SIZE_INDEX = 0x90,
 		INDEX_DIFF = 8;
 				
 	const uint32_t 
 		JDV_SIG_INDEX 	= searchFunc(Image_Vec, 0, 0, JDV_SIG),
-		PROFILE_SIG_INDEX = searchFunc(Image_Vec, 0, 0, PROFILE_SIG);
-		
+		PROFILE_SIG_INDEX = searchFunc(Image_Vec, 0, 0, PROFILE_SIG),
+		DATA_FILE_SIZE = getByteValue(Image_Vec, DATA_FILE_SIZE_INDEX);
+
 	if (JDV_SIG_INDEX == Image_Vec.size()) {
 		std::cerr << "\nImage File Error: Signature check failure. This is not a valid jdvrif file-embedded image.\n\n";
 		return 1;
 	}
 	
-	uint8_t extract_success_byte_val = Image_Vec[JDV_SIG_INDEX + INDEX_DIFF - 1];
+	uint8_t 
+		byte_check = Image_Vec[DATA_FILE_SIZE_INDEX + 4],
+		extract_success_byte_val = Image_Vec[JDV_SIG_INDEX + INDEX_DIFF - 1];
 
 	// Remove JPG header and the APP2 ICC Profile/segment header,
 	// also, any other segments that could be added by hosting sites (e.g. Mastodon), such as EXIF. 
@@ -53,7 +57,9 @@ int jdvOut(const std::string& IMAGE_FILENAME) {
 	
 	const uint32_t INFLATED_FILE_SIZE = inflateFile(Decrypted_File_Vec);
 
-	if (Decrypted_File_Vec.empty()) {	
+	bool isInflateFailure = Decrypted_File_Vec.empty() || INFLATED_FILE_SIZE != DATA_FILE_SIZE || byte_check != DECRYPTED_FILENAME[0];
+				 
+	if (isInflateFailure) {	
 		std::fstream file(IMAGE_FILENAME, std::ios::in | std::ios::out | std::ios::binary);
 		std::streampos failure_index = JDV_SIG_INDEX + INDEX_DIFF - 1;
 
@@ -74,7 +80,7 @@ int jdvOut(const std::string& IMAGE_FILENAME) {
 
 		file.close();
 
-		std::cerr << "\nFile Error: Invalid recovery PIN or file is corrupt.\n\n";
+		std::cerr << "\nFile Recovery Error: Invalid PIN or file is corrupt.\n\n";
 		return 1;
 	}
 
