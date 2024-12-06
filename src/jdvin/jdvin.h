26552bac1c6422e67d48613959442886610554dd
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-12-06 09:10:22 +0000

    Update jdvin.h

--- src/jdvin/jdvin.h
+++ src/jdvin/jdvin.h
@@ -11,7 +11,10 @@
 #include <string>
 #include <vector>
 #include <iterator>
+
+// https://github.com/madler/zlib
 #include <zlib.h>
+// Copyright (C) 1995-2017 Jean-loup Gailly and Mark Adler
 
 #include "profilesVec.cpp"
 #include "writeFile.cpp"
