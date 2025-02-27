0575702afb06b83761675448f6460e5194001aca
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-02-27 20:08:01 +0000

    Add files via upload

--- src/jdvin/jdvin.h
+++ src/jdvin/jdvin.h
@@ -13,11 +13,11 @@
 
 // This project uses libsodium (https://libsodium.org/) for cryptographic functions.
 #define SODIUM_STATIC
-#include <sodium.h>
+#include <C:\Users\Nick\source\repos\jdvin\libsodium\include\sodium.h>
 // Copyright (c) 2013-2025 Frank Denis <github@pureftpd.org>
 
 // https://github.com/madler/zlib
-#include <zlib.h>
+#include <C:\Users\Nick\source\zlib-1.3.1\zlib.h>
 // Copyright (C) 1995-2024 Jean-loup Gailly and Mark Adler
 
 #include "profilesVec.cpp"
@@ -43,16 +43,16 @@ T getByteValue(const std::vector<uint8_t>&, uint32_t);
 uint64_t encryptFile(std::vector<uint8_t>&, std::vector<uint8_t>&, std::string&);
 
 bool 
+	isCompressedFile(const std::string&),
+	hasValidFilename(const std::string&),
 	writeFile(std::vector<uint8_t>&),
 	segmentDataFile(std::vector<uint8_t>&, std::vector<uint8_t>&);
 
 void
+	validateFiles(const std::string&, const std::string&, ArgOption),
 	eraseSegments(std::vector<uint8_t>&),
 	deflateFile(std::vector<uint8_t>&, bool),
 	valueUpdater(std::vector<uint8_t>&, uint32_t, const uint64_t, uint8_t),
 	displayInfo();
 
 int jdvIn(const std::string&, std::string&, ArgOption, bool);
-bool isCompressedFile(const std::string&);
-bool hasValidFilename(const std::string&);
-void validateFiles(const std::string&, const std::string&, ArgOption);
