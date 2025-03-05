5839671006a5d277a718f07963ef05f37a9039d3
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-03-05 10:51:20 +0000

    Update fileChecks.cpp

--- src/jdvin/fileChecks.cpp
+++ src/jdvin/fileChecks.cpp
@@ -17,26 +17,26 @@ bool isValidImageExtension(const std::string& ext) {
     	return validExtensions.count(ext) > 0;
 }
 
-void validateFiles(const std::string& imageFile, const std::string& dataFile, ArgOption platformOption) {
-	std::filesystem::path imagePath(imageFile), dataPath(dataFile);
+void validateFiles(const std::string& image_file, const std::string& data_file, ArgOption platformOption) {
+	std::filesystem::path image_path(image_file), data_path(data_file);
 
-    	std::string imageExt = imagePath.extension().string();
+    	std::string image_ext = image_path.extension().string();
 
-    	if (!isValidImageExtension(imageExt)) {
+    	if (!isValidImageExtension(image_ext)) {
         	throw std::runtime_error("File Type Error: Invalid image extension. Only expecting \".jpg\", \".jpeg\", or \".jfif\".");
     	}
 
-    	if (!std::filesystem::exists(imagePath)) {
+    	if (!std::filesystem::exists(image_path)) {
         	throw std::runtime_error("Image File Error: File not found. Check the filename and try again.");
     	}
 
-    	if (!std::filesystem::exists(dataPath) || !std::filesystem::is_regular_file(dataPath)) {
+    	if (!std::filesystem::exists(data_path) || !std::filesystem::is_regular_file(data_path)) {
         	throw std::runtime_error("Data File Error: File not found or not a regular file. Check the filename and try again.");
     	}
 
     	constexpr uint8_t MINIMUM_IMAGE_SIZE = 134;
 
-    	if (MINIMUM_IMAGE_SIZE > std::filesystem::file_size(imagePath)) {
+    	if (MINIMUM_IMAGE_SIZE > std::filesystem::file_size(image_path)) {
         	throw std::runtime_error("Image File Error: Invalid file size.");
     	}
 
@@ -44,9 +44,9 @@ void validateFiles(const std::string& imageFile, const std::string& dataFile, Ar
 		MAX_SIZE_DEFAULT = 2ULL * 1024 * 1024 * 1024,   // 2GB (cover image + data file)
     		MAX_SIZE_REDDIT  = 20ULL * 1024 * 1024;         // 20MB ""
 
-    	const uintmax_t COMBINED_FILE_SIZE = std::filesystem::file_size(dataPath) + std::filesystem::file_size(imagePath);
+    	const uintmax_t COMBINED_FILE_SIZE = std::filesystem::file_size(data_path) + std::filesystem::file_size(image_path);
 	
-    	if (std::filesystem::file_size(dataPath) == 0) {
+    	if (std::filesystem::file_size(data_path) == 0) {
 		throw std::runtime_error("Data File Error: File is empty.");
     	}
 
