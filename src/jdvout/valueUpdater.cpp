a27f0dcc62d9c944b533c873edc29dacbc246220
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-03-05 16:21:55 +0000

    Update valueUpdater.cpp

--- src/jdvout/valueUpdater.cpp
+++ src/jdvout/valueUpdater.cpp
@@ -1,4 +1,3 @@
-// Writes updated values, such as segment size lengths and other values, into the relevant vector index locations.
 void valueUpdater(std::vector<uint8_t>& vec, uint32_t value_index, const uint64_t VALUE, uint8_t bits) {
 	while (bits) {
 		vec[value_index++] = (VALUE >> (bits -= 8)) & 0xff;
