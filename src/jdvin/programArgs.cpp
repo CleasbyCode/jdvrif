35d29342aa6495d0e62afc0817f42ef0673e5ce0
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-03-05 10:53:17 +0000

    Update programArgs.cpp

--- src/jdvin/programArgs.cpp
+++ src/jdvin/programArgs.cpp
@@ -5,8 +5,8 @@ enum class ArgOption {
 
 struct ProgramArgs {
 	ArgOption platform = ArgOption::Default;
-    	std::string imageFile;
-    	std::string dataFile;
+    	std::string image_file;
+    	std::string data_file;
 
     	static ProgramArgs parse(int argc, char** argv);
 };
@@ -22,16 +22,16 @@ ProgramArgs ProgramArgs::parse(int argc, char** argv) {
         	throw std::runtime_error("Usage: jdvin [-r] <cover_image> <secret_file>\n\t\bjdvin --info");
     	}
 
-    	uint8_t argIndex = 1;
+    	uint8_t arg_index = 1;
     	if (argc == 4) {
 		if (std::string(argv[1]) != "-r") {
             		throw std::runtime_error("Input Error: Invalid arguments. Expecting \"-r\" as the only optional argument.");
         	}
         	args.platform = ArgOption::Reddit;
-        	argIndex = 2;
+        	arg_index = 2;
     	}
 
-    	args.imageFile = argv[argIndex];
-    	args.dataFile = argv[++argIndex];
+    	args.image_file = argv[arg_index];
+    	args.data_file = argv[++arg_index];
     	return args;
 }
