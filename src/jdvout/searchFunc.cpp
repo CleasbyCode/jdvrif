6948e17c4d10b9c3ad6b433a0ecb62b5e44a3fad
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-03-05 16:21:39 +0000

    Update searchFunc.cpp

--- src/jdvout/searchFunc.cpp
+++ src/jdvout/searchFunc.cpp
@@ -1,4 +1,3 @@
-// Search and return a vector index position of given string signature.
 template <typename T, size_t N>
 uint32_t searchFunc(std::vector<uint8_t>& vec, uint32_t start_index, const uint8_t INCREMENT_SEARCH_INDEX, const std::array<T, N>& SIG) {
     return static_cast<uint32_t>(std::search(vec.begin() + start_index + INCREMENT_SEARCH_INDEX, vec.end(), SIG.begin(), SIG.end()) - vec.begin());
