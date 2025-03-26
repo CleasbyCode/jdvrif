18df6c0021417d3ad99bc937e25b5e2e5afc14ee
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-03-26 00:20:34 +0000

    Delete src/jdvout directory

--- src/jdvout/valueUpdater.cpp
+++
@@ -1,5 +0,0 @@
-void valueUpdater(std::vector<uint8_t>& vec, uint32_t value_index, const uint64_t VALUE, uint8_t bits) {
-	while (bits) {
-		vec[value_index++] = (VALUE >> (bits -= 8)) & 0xff;
-	}
-}
