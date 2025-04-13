599e7810b4f3228ea57cbad1da383d4d1370ce2d
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-04-13 20:04:02 +0100

    Update transcodeImage.cpp

--- src/jdvin/transcodeImage.cpp
+++ src/jdvin/transcodeImage.cpp
@@ -1,7 +1,7 @@
 // This software is based in part on the work of the Independent JPEG Group.
 // https://github.com/libjpeg-turbo/libjpeg-turbo
 
-void transcodeImage(std::vector<uint8_t>& image_vec, uint8_t quality_val) {
+void transcodeImage(std::vector<uint8_t>& image_vec, const uint8_t JPG_QUALITY_VAL) {
     tjhandle decompressor = tjInitDecompress();
     if (!decompressor) {
         throw std::runtime_error("tjInitDecompress() failed.");
@@ -31,7 +31,7 @@ void transcodeImage(std::vector<uint8_t>& image_vec, uint8_t quality_val) {
     unsigned long jpegSize = 0;
     int flags = TJFLAG_PROGRESSIVE;
     if (tjCompress2(compressor, decoded_image_vec.data(), width, 0, height, TJPF_RGB, 
-                    &jpegBuf, &jpegSize, TJSAMP_444, quality_val, flags) != 0) {
+                    &jpegBuf, &jpegSize, TJSAMP_444, JPG_QUALITY_VAL, flags) != 0) {
         tjDestroy(compressor);
         throw std::runtime_error(std::string("tjCompress2: ") + tjGetErrorStr());
     }
