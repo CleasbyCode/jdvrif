c57d697f0528ce97cab1e04795a87dfe288eaf87
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-05-23 02:13:57 +0100

    Update segmentsVec.h

--- src/jdvin/segmentsVec.h
+++ src/jdvin/segmentsVec.h
@@ -5,4 +5,4 @@
 
 extern std::vector<uint8_t> segment_vec;        
 extern std::vector<uint8_t> bluesky_exif_vec;   
-extern std::vector<uint8_t> bluesky_xmp_vec;    
+extern std::vector<uint8_t> bluesky_xmp_vec;
