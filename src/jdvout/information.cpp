dadb870b2d104d4c0ffff5ffb1e643b817cbd000
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-06-16 01:28:32 +0100

    Update information.cpp

--- src/jdvout/information.cpp
+++ src/jdvout/information.cpp
@@ -4,7 +4,7 @@
 void displayInfo() {
 	std::cout << R"(
 
-JPG Data Vehicle (jdvout v4.2). 
+JPG Data Vehicle (jdvout v4.3). 
 Created by Nicholas Cleasby (@CleasbyCode) 24/01/2023.
 
 jdvout is a CLI tool for extracting hidden data from a jdvin "file-embedded" JPG image. 
