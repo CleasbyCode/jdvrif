ab7322fd5bc4d1d532cc148e85c180eec8e89e7b
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-08-27 12:50:47 +0100

    Update fileChecks.h

--- src/fileChecks.h
+++ src/fileChecks.h
@@ -7,5 +7,5 @@
 bool isCompressedFile(const std::string&);
 bool hasValidImageExtension(const std::string&);
 bool hasValidFilename(const std::string&);
-void validateImageFile(std::string&, ArgMode, ArgOption, uintmax_t&, std::vector<uint8_t>&);
-void validateDataFile(std::string&, ArgOption, uintmax_t&, uintmax_t&, std::vector<uint8_t>&, bool&);
+void validateImageFile(std::string&, ArgMode, ArgOption, uintmax_t&, std::vector<uint_fast8_t>&);
+void validateDataFile(std::string&, ArgOption, uintmax_t&, uintmax_t&, std::vector<uint_fast8_t>&, bool&);
