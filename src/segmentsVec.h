5de9299e45f146cec132b5297e2b5675103f5d98
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-08-27 12:54:22 +0100

    Update segmentsVec.h

--- src/segmentsVec.h
+++ src/segmentsVec.h
@@ -4,8 +4,7 @@
 #include <cstdint>
 #include <string>
 
-extern std::vector<uint8_t> segment_vec;        
-extern std::vector<uint8_t> bluesky_exif_vec;   
-extern std::vector<uint8_t> bluesky_xmp_vec;    
+extern std::vector<uint_fast8_t> segment_vec;        
+extern std::vector<uint_fast8_t> bluesky_exif_vec;   
+extern std::vector<uint_fast8_t> bluesky_xmp_vec;    
 extern std::vector<std::string> platforms_vec;
-
