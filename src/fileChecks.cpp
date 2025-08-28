a44d13a07f2f615822294f5c91a1233f63c9b035
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-08-28 11:21:52 +0100

    Update fileChecks.cpp

--- src/fileChecks.cpp
+++ src/fileChecks.cpp
@@ -26,7 +26,7 @@ bool hasValidImageExtension(const std::string& ext) {
     return valid_extensions.count(ext) > 0;
 }
 
-void validateImageFile(std::string& cover_image, ArgMode mode, ArgOption platform, uintmax_t& cover_image_size, std::vector<uint_fast8_t>& cover_image_vec) {
+void validateImageFile(std::string& cover_image, ArgMode mode, ArgOption platform, uintmax_t& cover_image_size, std::vector<uint8_t>& cover_image_vec) {
 	std::filesystem::path image_path(cover_image);
 
     std::string image_ext = image_path.extension().string();
@@ -51,7 +51,7 @@ void validateImageFile(std::string& cover_image, ArgMode mode, ArgOption platfor
 
 	cover_image_size = std::filesystem::file_size(image_path);
 
-    constexpr uint_fast8_t MINIMUM_IMAGE_SIZE = 134;
+    constexpr uint8_t MINIMUM_IMAGE_SIZE = 134;
 
     if (MINIMUM_IMAGE_SIZE > cover_image_size) {
         throw std::runtime_error("Image File Error: Invalid file size.");
@@ -73,15 +73,15 @@ void validateImageFile(std::string& cover_image, ArgMode mode, ArgOption platfor
 		throw std::runtime_error("File Size Error: Image file exceeds maximum default size limit for jdvrif.");
 	}
 	
-	std::vector<uint_fast8_t> tmp_vec(cover_image_size);
+	std::vector<uint8_t> tmp_vec(cover_image_size);
 	
 	cover_image_ifs.read(reinterpret_cast<char*>(tmp_vec.data()), cover_image_size);
 	cover_image_ifs.close();
 	
 	cover_image_vec.swap(tmp_vec);
-	std::vector<uint_fast8_t>().swap(tmp_vec);
+	std::vector<uint8_t>().swap(tmp_vec);
 	
-	constexpr std::array<uint_fast8_t, 2>
+	constexpr std::array<uint8_t, 2>
 		IMAGE_START_SIG	{ 0xFF, 0xD8 },
 		IMAGE_END_SIG   { 0xFF, 0xD9 };
 
@@ -90,7 +90,7 @@ void validateImageFile(std::string& cover_image, ArgMode mode, ArgOption platfor
 	}
 }
 	
-void validateDataFile(std::string& data_filename, ArgOption platform, uintmax_t& cover_image_size, uintmax_t& data_file_size, std::vector<uint_fast8_t>& data_file_vec, bool& isCompressedFile) {
+void validateDataFile(std::string& data_filename, ArgOption platform, uintmax_t& cover_image_size, uintmax_t& data_file_size, std::vector<uint8_t>& data_file_vec, bool& isCompressedFile) {
 	std::filesystem::path data_path(data_filename);
 	
 	if (!hasValidFilename(data_path.string())) {
@@ -107,7 +107,7 @@ void validateDataFile(std::string& data_filename, ArgOption platform, uintmax_t&
 		throw std::runtime_error("Read File Error: Unable to read data file. Check the filename and try again.");
 	}
 	
-	constexpr uint_fast8_t DATA_FILENAME_MAX_LENGTH = 20;
+	constexpr uint8_t DATA_FILENAME_MAX_LENGTH = 20;
 	
 	data_filename = data_path.filename().string();
 
@@ -147,11 +147,11 @@ void validateDataFile(std::string& data_filename, ArgOption platform, uintmax_t&
 		throw std::runtime_error("File Size Error: Combined size of image and data file exceeds maximum default size limit for jdvrif.");
 	}
 	
-	std::vector<uint_fast8_t> tmp_vec(data_file_size);
+	std::vector<uint8_t> tmp_vec(data_file_size);
 
 	data_file_ifs.read(reinterpret_cast<char*>(tmp_vec.data()), data_file_size);
 	data_file_ifs.close();
 	
 	data_file_vec.swap(tmp_vec);
-	std::vector<uint_fast8_t>().swap(tmp_vec);
+	std::vector<uint8_t>().swap(tmp_vec);
 }
