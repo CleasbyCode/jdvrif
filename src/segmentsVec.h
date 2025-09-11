257b29805075cc8203705f2b65860c9aee7962c7
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-09-11 10:14:07 +0100

    Add files via upload

--- src/segmentsVec.h
+++ src/segmentsVec.h
@@ -6,5 +6,6 @@
 
 extern std::vector<uint8_t> segment_vec;        
 extern std::vector<uint8_t> bluesky_exif_vec;   
+extern std::vector<uint8_t> bluesky_pshop_vec;
 extern std::vector<uint8_t> bluesky_xmp_vec;    
 extern std::vector<std::string> platforms_vec;
