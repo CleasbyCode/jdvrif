65d7b59c1f24dcbc54ea74c4718b8818789d16a0
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2025-09-24 12:54:06 +0100

    Update compile_jdvrif.sh

--- src/compile_jdvrif.sh
+++ src/compile_jdvrif.sh
@@ -2,7 +2,7 @@
 
 # compile_jdvrif.sh
 
-g++ -std=c++20 jdvrif.cpp -Wall -O2 -lz -lsodium -lturbojpeg -s -o jdvrif
+g++ -std=c++20 jdvrif.cpp -Wall -O3 -lz -lsodium -lturbojpeg -s -o jdvrif
 
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Executable 'jdvrif' created."
