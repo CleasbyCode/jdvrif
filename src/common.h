f5a182825945d1a434add3cb5df80e6fe72e45f2
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-02-12 09:29:35 +0000

    Update common.h

--- src/common.h
+++ src/common.h
@@ -3,10 +3,40 @@
 
 #ifdef _WIN32
     #include "windows/libjpeg-turbo/include/turbojpeg.h"
+    // This software is based in part on the work of the Independent JPEG Group.
+	// Copyright (C) 2009-2024 D. R. Commander. All Rights Reserved.
+	// Copyright (C) 2015 Viktor Szathmáry. All Rights Reserved.
+	// https://github.com/libjpeg-turbo/libjpeg-turbo
+
     #include "windows/zlib-1.3.1/include/zlib.h"
+    // zlib.h -- interface of the 'zlib' general purpose compression library
+	// version 1.3.1, January 22nd, 2024
+
+	// Copyright (C) 1995-2024 Jean-loup Gailly and Mark Adler
+
+	// This software is provided 'as-is', without any express or implied
+	// warranty. In no event will the authors be held liable for any damages
+	// arising from the use of this software.
+
+	// Permission is granted to anyone to use this software for any purpose,
+	// including commercial applications, and to alter it and redistribute it
+	// freely, subject to the following restrictions:
+
+	// 1. The origin of this software must not be misrepresented; you must not
+	//    claim that you wrote the original software. If you use this software
+	//    in a product, an acknowledgment in the product documentation would be
+	//    appreciated but is not required.
+	// 2. Altered source versions must be plainly marked as such, and must not be
+	//    misrepresented as being the original software.
+	// 3. This notice may not be removed or altered from any source distribution.
+
+	// Jean-loup Gailly        Mark Adler
+	// jloup@gzip.org          madler@alumni.caltech.edu
 
     #define SODIUM_STATIC
     #include "windows/libsodium/include/sodium.h"
+    // This project uses libsodium (https://libsodium.org/) for cryptographic functions.
+	// Copyright (c) 2013-2025 Frank Denis <github@pureftpd.org>
 
     #include <conio.h>
 
