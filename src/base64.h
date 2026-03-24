16911702b4998ec6729fdd992645efa23f06e87c
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-03-24 21:01:56 +0000

    Delete src directory

--- src/base64.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-#include <span>
-
-void binaryToBase64(std::span<const Byte> binary_data, vBytes& output_vec);
-void appendBase64AsBinary(std::span<const Byte> base64_data, vBytes& destination_vec);
