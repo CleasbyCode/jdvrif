7a7681e389fb27aa136d9f9c26148e7a7a311468
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-04-17 12:10:56 +0100

    Delete src directory

--- src/compression.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "common.h"
-
-#include <span>
-
-void zlibCompressFileToPath(const fs::path& input_path, const fs::path& output_path);
-void zlibInflateToFile(std::span<const Byte> compressed_data, const fs::path& output_path);
