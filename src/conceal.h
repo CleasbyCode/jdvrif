5472b3a049c2e4c0d984a211e38dc3a68c78c648
Nicholas Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2026-05-31 19:57:40 +0100

    Add files via upload

---
+++ src/conceal.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "common.h"
+
+void concealData(vBytes& jpg_vec, Option option, const fs::path& data_file_path);
---
+++ src/pin_input.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cstdint>
+
+[[nodiscard]] std::uint64_t getPin();
